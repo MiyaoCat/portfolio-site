3cdedd90c8baadb557217f78a9f7772d3b865050
john <jjmiyao@gmail.com>
2022-08-23 20:30:17 -0700

    responsive and modular site structure done!

---
+++ index.php
@@ -0,0 +1,78 @@
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+
+	<meta property="og:image" content="[IMAGE URL]">
+	<meta name="description" content="[SHORT PAGE DESCRIPTION]">
+
+	<title>2</title>
+
+	<link rel="stylesheet" href="css/site.css">
+</head>
+
+<body>
+	<div class='background'>Background</div>
+
+	<header class="site-header">
+		<nav>
+			<ul>
+				<li><a href='#'>Contact Me</a></li>
+				<li><a href='#'>About Me</a></li>
+				<li><a href='#'>Projects</a></li>
+			</ul>
+		</nav>
+	</header>
+
+	<section class='page-header'>
+		<div class='heading-wrap'>
+			<h1 class="title1">CASE</h1>
+			<h1 class="title2">EXERCISES</h1>
+		</div>
+	</section>
+	
+	<section class='page-content'>
+		<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus asperiores, fugiat repellendus explicabo perferendis, omnis beatae expedita accusantium tempora laborum.</p>
+	
+		<!-- 	<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus asperiores, fugiat repellendus explicabo perferendis, omnis beatae expedita accusantium tempora laborum.</p>
+	
+		<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus asperiores, fugiat repellendus explicabo perferendis, omnis beatae expedita accusantium tempora laborum.</p> -->
+	</section>
+
+	<!-- <section class='cta'>
+		<div class='button'>CTA</div>
+	</section> -->
+
+	<section class='bonus-content'>
+			
+	</section>
+
+	<footer class="site-footer">
+		<div class='logo'>LOGO</div>
+	</footer>
+</body>
+
+</html>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
