bd6df4cd2df4a1a55d74c0cd93b00b8055afeb4b
john <jjmiyao@gmail.com>
2022-08-27 14:04:46 -0700

    Added 404 page

---
+++ pages/404.php
@@ -0,0 +1,7 @@
+<?php  
+	$title1 = "404!";
+	include("modules/page-header/page-header.php");
+
+	$content = "That's the internet police code for page not found. Why 404? Couldn't tell ya, but I recommend going back to a real page.";
+	include("modules/generic-text/generic-text.php");
+?>
