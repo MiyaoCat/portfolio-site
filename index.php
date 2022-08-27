6bbca656e8ba0836b5ff958360c52c8045baa48d
john <jjmiyao@gmail.com>
2022-08-26 18:37:25 -0700

    Refined page router, placed resume button on about page

--- index.php
+++ index.php
@@ -10,30 +10,34 @@
 		if ( isset($_GET["page"]) ) {
 			$page = $_GET["page"];
 		}
-	
-		if ($page == "home") {
-			include("pages/home.php");
+		
+		if ($page = $_GET["page"]) {
+			include("pages/$page.php");
 		}
+		
+		// if ($page == "home") {
+		// 	include("pages/home.php");
+		// }
 	
-		if ($page == "contact") {
-			include("pages/contact.php");
-		}
+		// if ($page == "contact") {
+		// 	include("pages/contact.php");
+		// }
 	
-		if ($page == "about") {
-			include("pages/about.php");
-		}
+		// if ($page == "about") {
+		// 	include("pages/about.php");
+		// }
 	
-		if ($page == "projects") {
-			include("pages/projects.php");
-		}
+		// if ($page == "projects") {
+		// 	include("pages/projects.php");
+		// }
 
-		if ($page == "style-guide") {
-			include("pages/style-guide.php");
-		}
+		// if ($page == "style-guide") {
+		// 	include("pages/style-guide.php");
+		// }
 
-		if ($page == "resume") {
-			include("pages/resume.php");
-		}
+		// if ($page == "resume") {
+		// 	include("pages/resume.php");
+		// }
 	?>
 
 
