45a9884b12b7b4ad6adf08d6edfeaa67ca572090
john <jjmiyao@gmail.com>
2022-08-31 16:40:27 -0700

    test push

--- pages/default.php
+++ pages/default.php
@@ -1,8 +1,10 @@
 <?php foreach ($pageData["sections"] as $sections) {
 	$module = $sections["module"];
+	$link = "?page=module&slug=$sections[module]";
 ?>
 
 	<section class="<?=$page?> <?=$module?>">
+		<a href="<?=$link?>" target="<?=$module?>">Detail</a>
 		<?php include("modules/$module/$module.php"); ?>
 	</section>
 
---
+++ pages/module.php
@@ -0,0 +1,7 @@
+<?php $moduleName = $_GET["slug"]; ?>
+
+<section class="<?=$page?> <?=$moduleName?>">
+	<?php 
+		include("modules/$moduleName/$moduleName.php");
+	?>
+</section>
