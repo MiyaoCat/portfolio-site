f5aadab1349cedd76419af98174b63076cc9e0c8
john <jjmiyao@gmail.com>
2022-09-02 14:49:12 -0700

    Organized code

--- modules/project-card/project-card.php
+++ modules/project-card/project-card.php
@@ -11,27 +11,29 @@
 		<div class="project-title">
 
 			<?php  
-				if ($slug !== "exercises") {
-			?>
-			<a href="?page=project&slug=<?=$slug?>">
-				<h2 class="loud-voice"><?=$title1?></h2>
-			</a>
+				if ($slug !== "exercises") { ?>
 
+				<a href="?page=project&slug=<?=$slug?>">
+					<h2 class="loud-voice"><?=$title1?></h2>
+				</a>
 			<?php } ?>
 
 			<?php  
-				if ($slug == "exercises") {
-			?>
+				if ($slug == "exercises") { ?>
+
 				<a href="?page=exercises">
 					<h2 class="loud-voice"><?=$title1?></h2>
 				</a>
 			<?php } ?>
+
 		</div>
 
 		<ul class="languages">
+
 			<?php foreach ($project["languages"] as $languages) { ?>
 				<li><?=$languages?></li>
 			<?php } ?>
+			
 		</ul>
 
 		<p class="normal-voice"><?=$intro?></p>
