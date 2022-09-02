24763896c106c7278f3a035a0686cb8a2e739187
john <jjmiyao@gmail.com>
2022-09-01 18:49:05 -0700

    Adjusted columns so page header heights are uniform

--- pages/projects.php
+++ pages/projects.php
@@ -1,17 +1,20 @@
+<?php foreach ($pageData["sections"] as $sections) {
+	$module = $sections["module"];
+	$link = "?page=module&slug=$sections[module]";
+?>
+
+	<section class="<?=$module?>">
+		<?php styleGuideDetailLink($module, $pageData["slug"]); ?>
+		<?php include("modules/$module/$module.php"); ?>
+	</section>
+
+<?php } ?>
 
 <?php 
 	$json = file_get_contents("data/projects.json");
 	$projects = json_decode($json, true);
 ?>	
 
-<header class="<?=$page?> page-header">
-		<?php include("modules/page-header/page-header.php"); ?>
-</header>	
-
-<!-- <section class="intro">
-		<p><?=$pageData["intro"]?></p>
-</section>
- -->
 <section class="<?=$page?> project-card">
 	<?php 
 
