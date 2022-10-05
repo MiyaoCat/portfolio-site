b21b52453f23dcf5b1700b8f36c853000dcb59c1
john <jjmiyao@gmail.com>
2022-10-04 18:20:32 -0700

    Form fix3

--- modules/email-form/email-form.php
+++ modules/email-form/email-form.php
@@ -1,20 +1,17 @@
 <?php  
 	if ( isset($_POST["submitted"]) ) {
 
-	echo "<pre>";
-		print_r($_POST);
-	echo "</pre>";
-
 	$email = $_POST['inputEmail'];
 	$subject = $_POST['inputSubject'];
 	$message = $_POST['inputMessage'];
 
 	$to = "dftwjohn@gmail.com";
+
 	$body = "";
 
-	$body .= "Email: " . $email . "\r\n";
-	$body .= "Subject: " . $subject . "\r\n";
-	$body .= "Message: " . $message . "\r\n";
+	$body = "Email: " . $email . "\r\n";
+	$body = "Subject: " . $subject . "\r\n";
+	$body = "Message: " . $message . "\r\n";
 
 
 	mail($to, $subject, $body);
@@ -24,14 +21,31 @@
 <form method="POST">
 	
 	<label for="inputEmail">Email</label>
-	<input type="email" name="inputEmail" placeholder="youremail@email.com" required>
+	<input type="email" name="inputEmail" required>
+
 	<label for="inputSubject">Subject</label>
 	<input type="text" name="inputSubject">
 
 	<label for="inputMessage">Message</label>
-	<textarea rows="10" columns="8" name="inputMessage">
+	<textarea rows="8" name="inputMessage" required>
 	</textarea>
 
-	<button type="submit" name="submitted">Submit</button>
+	<button class="contact-form" type="submit" name="submitted">Submit</button>
 
 </form>
+
+<?php  
+	if ( isset($_POST["submitted"]) ) {
+
+		if ( isset($_POST["inputEmail"]) ) {
+
+		if ( isset($message) ) {	
+
+			$message !== "";
+?>
+			<p class="normal-voice">Thank you for your message. I will get back to you ASAP!</p>
+		<?php } ?>
+		
+		<?php } ?>
+
+<?php } ?>
