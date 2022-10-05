2901dc205527a9665274e5a4b7fe48c81f49cfdd
john <jjmiyao@gmail.com>
2022-10-04 17:01:15 -0700

    Created email form on contact page

---
+++ modules/email-form/email-form.php
@@ -0,0 +1,39 @@
+<?php  
+	if ( isset($_POST["submitted"]) ) {
+		echo "<p class='return-message'>Your message has been received </p>";
+	
+
+	echo "<pre>";
+		print_r($_POST);
+	echo "</pre>";
+
+	$email = $_POST['inputEmail'];
+	$subject = $_POST['inputSubject'];
+	$message = $_POST['inputMessage'];
+
+	$to = "dftwjohn@gmail.com";
+	$body = "";
+
+	$body = "Email: " . $email . "\r\n";
+	$body = "Subject: " . $subject . "\r\n";
+	$body = "Message: " . $message . "\r\n";
+
+
+	mail($to, $subject, $message);
+}
+?>
+
+<form action="email-form.php" method="POST">
+	
+	<label for="inputEmail">Email</label>
+	<input type="email" name="inputEmail" placeholder="youremail@email.com">
+	<label for="inputSubject">Subject</label>
+	<input type="text" name="inputSubject">
+
+	<label for="inputMessage">Message</label>
+	<textarea rows="10" columns="8" name="inputMessage">
+	</textarea>
+
+	<button type="submit" name="submitted">Submit</button>
+
+</form>
