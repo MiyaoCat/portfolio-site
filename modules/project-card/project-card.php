84e2a22b271aa8d0b03c420358c2480297a45802
john <jjmiyao@gmail.com>
2022-10-07 14:47:21 -0700

    Formatted projects cards, generic text

--- modules/project-card/project-card.php
+++ modules/project-card/project-card.php
@@ -8,35 +8,39 @@
 
 <project-card class="<?=$slug?>">
 	
-		<div class="project-title">
-
-			<?php  
-				if ($slug !== "exercises") { ?>
-
-					<a href="?page=project&slug=<?=$slug?>">
-						<h2 class="loud-voice"><?=$title1?></h2>
-					</a>
-			<?php } ?>
-
-			<?php  
-				if ($slug == "exercises") { ?>
-					
-					<a href="?page=exercises">
-						<h2 class="loud-voice"><?=$title1?></h2>
-					</a>
-			<?php } ?>
-
+		<div class="project-details">
+			<div class="project-title">
+			
+				<?php  
+					if ($slug !== "exercises") { ?>
+			
+						<a href="?page=project&slug=<?=$slug?>">
+							<h2 class="loud-voice"><?=$title1?></h2>
+						</a>
+				<?php } ?>
+			
+				<?php  
+					if ($slug == "exercises") { ?>
+						
+						<a href="?page=exercises">
+							<h2 class="loud-voice"><?=$title1?></h2>
+						</a>
+				<?php } ?>
+			
+			</div>
+			
+			<ul class="languages">
+			
+				<?php foreach ($project["languages"] as $languages) { ?>
+					<li class="calm-voice"><?=$languages?></li>
+				<?php } ?>
+			
+			</ul>
+			
+			<div class="intro">
+				<p class="normal-voice"><?=$intro?></p>
+			</div>
 		</div>
-
-		<ul class="languages">
-
-			<?php foreach ($project["languages"] as $languages) { ?>
-				<li class="calm-voice"><?=$languages?></li>
-			<?php } ?>
-
-		</ul>
-
-		<p class="normal-voice intro"><?=$intro?></p>
 
 		<div class="project-images">
 			<picture class="project-image">
