6e8ecbdba6a38b9156ec62e381edbf04ad642672
john <jjmiyao@gmail.com>
2022-10-07 17:52:10 -0700

    Fixed hover state in dark mode

--- modules/social-icons/social-icons.php
+++ modules/social-icons/social-icons.php
@@ -1,23 +1,23 @@
 <social-icons class="<?=$page?>">
 	<ul>
 		<li class="logo">
-			<a href="#">
+			<a href="https://github.com/MiyaoCat" target="_blank">
 				<?php include("./images/icons/github-circle.svg"); ?>
 			</a>
 		</li>
 		<li class="logo">
-			<a href="#">
+			<a href="https://twitter.com/MiyaoCodes" target="_blank">
 				<?php include("./images/icons/twitter-large.svg"); ?>
 			</a>
 		</li>
 		<li class="logo">
-			<a href="#">
+			<a href="https://www.linkedin.com/in/johnmiyao/" target="_blank">
 				<?php include("./images/icons/linkedin-letters.svg"); ?>
 			</a>
 		</li>
 
 		<li class="logo">
-			<a href="#">
+			<a href="https://substack.com/profile/19685771-john-learns-to-code?utm_source=user-menu" target="_blank">
 				<?php include("./images/icons/substack-short.svg"); ?>
 			</a>
 		</li>
