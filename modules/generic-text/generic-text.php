4071d806cf1577225d9310ffacd196aaa03e48dc
john <jjmiyao@gmail.com>
2022-10-10 11:15:04 -0700

    Made content dynamic based on page

--- modules/generic-text/generic-text.php
+++ modules/generic-text/generic-text.php
@@ -1,12 +1,22 @@
-<?php
-	if ($page == "project") { 
-	$content = $CaseStudymodule["content"] ?? "<p>This is generic content text for the generic-text module. Hope you enjoyed reading!</p>";
+<?php 
+	if ($page !== "project") {
+		$content = $sections["content"] ?? "<p>This is generic content text for the generic-text module. Hope you enjoyed reading!</p>";
 ?>
 
-<generic-text>
-	<p class="normal-voice"><?=$content?></p>
-</generic-text>
+		<generic-text>
+			<p class="normal-voice"><?=$content?></p>
+		</generic-text>
 
+
+
+<?php
+	} elseif ($page == "project") { 
+		$content = $CaseStudymodule["content"] ?? "<p>This is generic text for the individual project pages. Hope you enjoyed reading!</p>";
+?>
+
+		<generic-text>
+			<p class="normal-voice"><?=$content?></p>
+		</generic-text>
 
 <?php } ?>
 
