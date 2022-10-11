aefceac2700a511a7d2efbcbb9688459c30f3f62
john <jjmiyao@gmail.com>
2022-10-10 18:53:33 -0700

    Added dynamic filepath to img source

--- modules/figure/figure.php
+++ modules/figure/figure.php
@@ -1,12 +1,33 @@
 <?php 
-	$filePath = $CaseStudymodule["filePath"];
+	if ($page !== "project") {
+
+		$filePath = $sections["filePath"];
+		$altText = $sections["altText"];
 ?>
 
 <figure>
 	<picture>
-		<img src="images/selfie-gradient.png" alt="">
+		<img src="<?=$filePath?>" alt="<?=$altText?>">
 	</picture>
-
-	<p><?=$filePath?></p>
 </figure>	
 
+<?php 
+	} elseif ($page == "project") {
+
+	$filePath = $CaseStudyModule["filePath"];
+	$altText = $CaseStudyModule["altText"];
+	$caption = $CaseStudyModule["caption"];
+
+?>
+
+<figure>
+	<picture>
+		<img src="<?=$filePath?>" alt="<?=$altText?>">
+	</picture>
+
+	<image-caption>
+		<p class="calm-voice"><?=$caption?></p>
+	</image-caption>
+</figure>	
+
+<?php } ?>
