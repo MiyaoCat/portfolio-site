92ffaeff0cceb754b103e133e15ebf3b241e1b2a
john <jjmiyao@gmail.com>
2022-10-12 20:31:52 -0700

    Fixed function error

--- exercises/simple-math.php
+++ exercises/simple-math.php
@@ -1,6 +1,9 @@
 
 	<h1 class="loud-voice">Let's do Math!</h1>
-	<p class="regular-voice">Just type in your numbers and watch the magic happen!</p>
+	<div class="instructions">
+		<p class="regular-voice">Just type in your numbers and watch the magic happen!</p>
+	</div>
+
 	<?php 
 			$number1 = "";
 			$number2 = "";
@@ -11,11 +14,13 @@
 
 			if ( isset ($_POST["number2"]) ) {
 				$number2 = $_POST["number2"];
+
+				$multiply = $number1 * $number2;
+			$divide = $number1 / $number2;
+			$add = $number1 + $number2;
 			}
 
-			// $multiply = ($number1) * ($number2);
-			// $divide = ($number1) / ($number2);
-			// $add = ($number1) + ($number2);
+			
 
 		?>
 		
@@ -27,21 +32,27 @@
 			value="<?=$number1?>"
 			min="0"
 			placeholder="Enter your first number"
-			label="write your number">
+			label="write your number"
+			required>
 
 		<input 
 			type="number" 
 			name="number2" 
 			value="<?=$number2?>"
-			placeholder="Enter your second number">
+			placeholder="Enter your second number"
+			required>
 
-		<div class="instructions">
 			<button type="submit" name="submitted">Submit</button>
 			
-			<p>Your total is: <span class="special"><?=$multiply?></span></p>
-			<p>Your total is: <span class="special"><?=$divide?></span></p>
-			<p>Your total is: <span class="special"><?=$add?></span></p>
-		</div>
+			
+
 
 	</form>
 
+<output>
+	<?php if ( isset($_POST["submitted"]) ) { ?>	
+		<p>Your total is: <span class="special"><?=$multiply?></span></p>
+		<p>Your total is: <span class="special"><?=$divide?></span></p>
+		<p>Your total is: <span class="special"><?=$add?></span></p>
+	<?php } ?>	
+</output>
