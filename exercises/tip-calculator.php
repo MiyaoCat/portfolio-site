27115725545666739555855cdc399393eb936652
john <jjmiyao@gmail.com>
2022-10-18 14:21:58 -0700

    Fixed error

--- exercises/tip-calculator.php
+++ exercises/tip-calculator.php
@@ -10,13 +10,14 @@ $taxOutput="";
 $total = 0;
 $totalOutput = "";
 
-
 $submittedPOST = isset($_POST['submitted']);
 
-if ($submittedPOST)
+if ($submittedPOST) {
+
 // Get the order amount and the state from the form below)
 $subtotal = round($_POST['subtotal'], 2);
 $state = $_POST['state'];
+
 
 //if the state is WI
 if ($state == "WI") {
@@ -27,26 +28,23 @@ if ($state == "WI") {
 	$taxOutput = "<p>The subtotal is \$" . number_format($subtotal,2) . ".</p> <p>The tax is \$" . number_format($tax,2) . ".</p>";
 }
 
-
-
-
-	//display subtotal and tax
+//display subtotal and tax
 
 //for all states
 	//show total
 
 $total = $tax + $subtotal;
 $totalOutput = "$taxOutput <p>The total is \$" . number_format($total, 2) . ".</p>";
-
+}
 ?>
 
 
-<h1>Exercise- Tax Calculator</h1>
-<h2>Let us know the amount of your order, and what state you live in. We'll let you know the amount of state sales tax (WI only) and your grand total.</h2>
+
 
 
 <form method="POST">
-
+<h1 class="loud-voice">Exercise- Tax Calculator</h1>
+<h2>Let us know the amount of your order, and what state you live in. We'll let you know the amount of state sales tax (WI only) and your grand total.</h2>
 	<div class="field">
 		<label for="">What is the order amount?</label>
 		<input type="number" name="subtotal" value="<?=$subtotal?>" min=0 required>
@@ -69,17 +67,19 @@ $totalOutput = "$taxOutput <p>The total is \$" . number_format($total, 2) . ".</
 
 
 
-<?php
-
-	//array checker to monitor inputs
-	function format($variable) {
-		echo "<pre>";
-			echo "<code>";
-				print_r( $variable );
-			echo "</code>";
-		echo "</pre>";
-	}
-
-	format( $_POST );
-
-?>
+<div class="array">
+	<?php
+	
+		//array checker to monitor inputs
+		function format($variable) {
+			echo "<pre>";
+				echo "<code>";
+					print_r( $variable );
+				echo "</code>";
+			echo "</pre>";
+		}
+	
+		format( $_POST );
+	
+	?>
+</div>
