5f20ec46d986e7226c23c1d4932b727adac5000f
john <jjmiyao@gmail.com>
2022-10-18 11:18:10 -0700

    Added characater count. Fixed form function

--- exercises/count-characters.php
+++ exercises/count-characters.php
@@ -1,39 +1,45 @@
 
-			<form method="POST">
-				<h1 class="loud-voice">Character Count</h1>
-				<div class="instructions">
-				
-				<p class="regular-voice">Type in a string of text and I'll count how many characters you entered. </p>
+<form method="POST">
+<h1 class="loud-voice">Character Count</h1>
 
-				<input 
-					type="text" 
-					name="string" 
-					value="<?=$string?>" 
-					placeholder="Type whatever you want">
-				<button type="submit" name="enter">Submit</button>
-			</div>
+ 	
+	<div class="instructions">
+		<p class="regular-voice">Type in a string of text and I'll count how many characters you entered. </p>
+	</div>
 
-		 	<?php 
-			//setup. Establish variables
-			$string = "";
+	<input 
+		type="text" 
+		name="string" 
+		value="" 
+		placeholder="Type whatever you want">
 
-			//If there's a form submission. Reassign values
-			if ( isset($_POST["string"]) ) {
-				 $string = $_POST["string"];
-				 $message = "The $string is $length long";
-			
-
-			//calculations
-			$length = strlen($string);
-
-			$message = "You entered <span>$string</span>. It is <span class='length'>$length</span> characters long.";
-
-		 ?>
-		 	
-		 	<p class="regular-voice"><?=$message?></p>
-		 	
-
-		 </form>
-		 <?php } ?>
+	<button type="submit" name="enter">Submit</button>
+ </form>
 
 
+ 	
+<output>
+<?php 
+	//setup. Establish variables
+	$string = "";
+	$length = "";
+	$lengthWithSpaces = "";
+
+	//If there's a form submission. Reassign values
+	if ( isset($_POST["string"]) ) {
+		 $string = $_POST["string"];
+	//calculations
+	$length = strlen($string) - substr_count($string, ' ');
+	$lengthWithSpaces = strlen($string);
+	$word = str_word_count($string, 0);
+
+	//Return message
+	$message = "You entered <span>$string</span>. It is <span class='length'>$length</span> characters long without spaces";
+
+ ?>
+	<p class="regular-voice"><?=$message?></p><br>
+	<p class="regular-voice">Or it's <?=$lengthWithSpaces?> characters WITH spaces.</p>
+	<br>
+	<p class="regular-voice">Or it's <?=$word?> word(s).</p>
+<?php } ?>
+</output>
