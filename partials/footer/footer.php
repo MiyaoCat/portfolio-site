d2f8b054ef0beb223a01fbef7136e70b2784f68b
john <jjmiyao@gmail.com>
2022-10-20 10:51:00 -0700

    Added logo to header; adjusted logo size

--- partials/footer/footer.php
+++ partials/footer/footer.php
@@ -1,15 +1,17 @@
 <footer class="site-footer">
-		<div class="query-string"><?php echo "?" . queryString(); ?></div>
-		<div class='logo'>
-			<a href="?page=home">
-				<?php include("./images/icons/logo.svg"); ?>
-			</a>
-		</div>
+	<div class="query-string"><?php echo "?" . queryString(); ?></div>
+	
+	<div class='logo'>
+		<a href="?page=home">
+			<?php include("./images/icons/logo.svg"); ?>
+		</a>
+	</div>
 
-		<a href="?page=style-guide">Style Guide</a>
-		<a href="?page=resume">Resume</a>
-		<a href="?page=exercises">Exercises</a>
+	<a href="?page=style-guide">Style Guide</a>
+	<a href="?page=resume">Resume</a>
+	<a href="?page=exercises">Exercises</a>
 	</footer>
+	
 </body>
 
 </html>
