37b9a7d31594a14b736de394436bfe3f51918331
john <jjmiyao@gmail.com>
2022-11-10 15:13:34 -0800

    Styling style-guide first draft

--- pages/default.php
+++ pages/default.php
@@ -1,13 +1,33 @@
 <?php foreach ($pageData["sections"] as $sections) {
 
 	$module = $sections["module"];
-	$link = "?page=module&slug=$sections[module]";
+	// $link = "?page=module&slug=$sections[module]";
 ?>
 
 
 	<section class="<?=$module?>">
 		<?php styleGuideDetailLink($module, $pageData["slug"]); ?>
 
+		<?php include("modules/$module/$module.php"); ?>
+	</section>
+
+<?php } ?>
+
+<?php 
+	//This is for the separate figure page (after clicking 'detail') within the style guide
+	if ($page == "module") {
+		$json = file_get_contents("data/page-data/style-guide.json");
+		$styleGuideData = json_decode($json, true);
+
+		foreach ($styleGuideData["sections"] as $modules) {
+			if ($modules["module"] == "figure") {
+				$filePath = $modules["filePath"];
+				$altText = $modules["altText"];
+			}
+		}
+?>
+	
+	<section class="<?=$module?>">
 		<?php include("modules/$module/$module.php"); ?>
 	</section>
 
