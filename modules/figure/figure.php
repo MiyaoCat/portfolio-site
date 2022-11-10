d3df85a1014a2543624ebf1d55c3c1d4bf5bf75f
john <jjmiyao@gmail.com>
2022-11-10 12:12:06 -0800

    Fixed image render

--- modules/figure/figure.php
+++ modules/figure/figure.php
@@ -1,13 +1,16 @@
 <?php 
+	//This is for the separate figure page (after clicking 'detail') within the style guide
 	if ($page == "module") {
 		if ($moduleName == "figure") {
 			$json = file_get_contents("data/page-data/style-guide.json");
 			$styleGuideData = json_decode($json, true);
 
 			foreach ($styleGuideData["sections"] as $modules) {
+				if ($modules["module"] == "figure") {
 					$filePath = $modules["filePath"];
 					$altText = $modules["altText"];
 				}
+			}
 		}		
 ?>
 
