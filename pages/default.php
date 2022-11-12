935ec82168cdc10bbbd31826f99e3e76d9064a1e
john <jjmiyao@gmail.com>
2022-11-12 09:28:18 -0800

    Removed redundant code

--- pages/default.php
+++ pages/default.php
@@ -3,27 +3,6 @@
 ?>
 
 	<section class="<?=$module?>">
-		<?php styleGuideDetailLink($module, $pageData["slug"]); ?>
-		<?php include("modules/$module/$module.php"); ?>
-	</section>
-
-<?php } ?>
-
-<?php 
-	//This is for the figure page (after clicking 'detail') within the style guide
-	if ($page == "module") {
-		$json = file_get_contents("data/page-data/style-guide.json");
-		$styleGuideData = json_decode($json, true);
-
-		foreach ($styleGuideData["sections"] as $modules) {
-			if ($modules["module"] == "figure") {
-				$filePath = $modules["filePath"];
-				$altText = $modules["altText"];
-			}
-		}
-?>
-	
-	<section class="<?=$module?>">
 		<?php include("modules/$module/$module.php"); ?>
 	</section>
 
