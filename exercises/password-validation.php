ebfbf77790b206b9bfd10e6ada1b6bb6fc37fe08
john <jjmiyao@gmail.com>
2022-11-30 22:33:30 -0800

    Hide initial questions (still needs work)

--- exercises/password-validation.php
+++ exercises/password-validation.php
@@ -7,47 +7,60 @@
 ?>
 
 <form method="POST">
-	<label for="fruit">What is your favorite fruit?</label>
-	<input type="text" name="fruit" value="<?=$fruit?>">
-	
-	<label for="number">What is your favorite number?</label>
-	<input type="number" name="number" value="<?=$number?>">
+
+	<?php  
+		// $hide = "";
+		// $unHide = "";
+
+		// if ( isset($_POST["fruit"]) AND isset($_POST["number"]) ) {
+		// 	$hide = "style='display:none;'"; 
+		// }
+
+		// elseif ( isset($_POST["password"]) ) {
+		// 	if ($_POST["password"] !== $_POST["createdPassword"]) {
+		// 		$unHide = "style='display:flex'";
+		// 	}
+		// }	
+
+	?>
+		<div class="password-create">
+			<label for="fruit">What is your favorite fruit?</label>
+			<input type="text" name="fruit" value="<?=$fruit?>">
+			
+			<label for="number">What is your favorite number?</label>
+			<input type="number" name="number" value="<?=$number?>">
+		</div>
+
 
 	<?php  
 	if ($submitted) {
 		$fruit = $_POST["fruit"];
 		$number = $_POST["number"];
+		$password = $fruit . $number;
 		$passwordEnter = "";
+
+		if ( isset($_POST["password"]) ) {
+			if ($_POST["password"] == $password) {
+				echo "correct";
+			}
+		};
+
 	?>
 		<output>
 			<p class="normal-voice">You entered:</p>
-			<?php  $password = $fruit . $number; ?>	
+			<?php  $password = $fruit . $number; ?>		
 			<?=$password?>	
+			
 			<p class="normal-voice">This will be your password</p>
 		</output>
 
 		<form-wrap>
 			<label for="password">Please enter your password</label>
 			<input type="text" name="password" value="<?=$passwordEnter?>">
+			<input class="hidden" type="text" name="createdPassword" value="<?=$password?>">
 		</form-wrap>
 
-		<?php  
-			$passwordEnter = "";
-			if ($submitted) {
-				if ($passwordEnter == $password) {
-		?>
-			<output>
-				Correct!
-			</output>
-				<?php } ?>
-			<?php } 
-				else { ?>
 
-			<output>
-				wrong!
-			</output>
-					
-			<?php } ?>		
 	<?php } ?>
 	<button type="submit" name="submitted">Submit</button>
 
