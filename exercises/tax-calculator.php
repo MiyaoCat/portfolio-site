7103bc8bcf31cbd29fde1498b97559557ba366d0
john <jjmiyao@gmail.com>
2022-12-05 00:37:39 -0800

    Data backed tax info to JSON file

--- exercises/tax-calculator.php
+++ exercises/tax-calculator.php
@@ -1,83 +1,31 @@
 <?php  
-//test
-// What is the order amount? 10
-// What is the state? WI
-// The subtotal is $10.00.
-// The tax is $0.55.
-// The total is $10.55.
+$json = file_get_contents("data/exercises/tax-calculator.json");
+$taxData = json_decode($json, true);
 
-// Or
+$submitted = isset($_POST["submitted"]);
+$stateSelected = "";
+$order = "";
+$orderMessage = "";
+$message = "";
 
-// What is the order amount? 10
-// What is the state? MN
-// The total is $10.00
 
-// Prompt for the order amount 
-// Prompt for the state
-// If the sate is WI then the tax is 5.5.
-// Output is order amount * tax + order amount
 
-// If the state is NOT WI 
-// Then only supply the order amount. 
+if($submitted) {
+	$order = $_POST["order-amount"];
 
-	$state = "";
-	$total = 0;
-	$order = 0;
-	$tax = 0;
-	$message = "";
-	$orderMessage = "";
-	$submitted = isset($_POST["submitted"]);
+	foreach($taxData as $states) {
+		$state = $states["state"];
 
-	if ( $submitted ) {
-		$order = $_POST["order-amount"];
-		$state = $_POST["state-selected"];
-
-		if ($state == "CA") {
-			$tax = .0725;
-		};
-
-		if ($state == "FL") {
-			$tax = .06;
-		};
-
-		if ($state == "HI") {
-			$tax = .04;
-		};
-
-		if ($state == "NY") {
-			$tax = .04;
-		};
-
-		if ($state == "OR") {
-			$tax = 0;
-		};
-
-		if ($state == "WA") {
-			$tax = .065;
-		};
-
-		if ($state == "WI") {
-			$tax = .05;
-		};
-
-		if ($state == "WY") {
-			$tax = .04;
-		} 
-	} 
+		$tax = $states["tax"] / 100;
 	
-	$total = number_format($order * (1 + $tax), 2);
-
-	if ($submitted) {
-		if ($state == "") {
-			$message = "<p class='calm-voice warning'>You didn't select a state!</p>";
-		}
-	}	
-
-	if ($submitted) {
-		if ($order <= 0) {
-			$orderMessage = "<p class='calm-voice warning'>Either you're stealing or it's free</p>";
+		if ($_POST["state-selected"] == $state) {
+			echo "<output>" . $state . " " . $tax . "</output>";
 		}
 	}
+}
+
+$total = number_format($order * (1 + $tax), 2);
+
 ?>
 
 <form method="POST">
@@ -101,13 +49,8 @@
 		<option value="">--Select a State--</option>
 
 		<option value="CA">CA</option>
-		<option value="FL">FL</option>
-		<option value="HI">HI</option>
-		<option value="NY">NY</option>
 		<option value="OR">OR</option>
 		<option value="WA">WA</option>
-		<option value="WI">WI</option>
-		<option value="WY">WY</option>
 	</select>
 
 	
@@ -123,12 +66,11 @@
 
 <?php  
 	if ( $submitted ) {
-
 		if ($order > 0) {
-		if ($state !== "") {
+		if ($_POST["state-selected"] !== "") {
 ?>
 	<output>
-		<p class="normal-voice"><?=$state?> has a tax rate of <?=100*$tax?>%</p>
+		<p class="normal-voice"><?=$_POST["state-selected"]?> has a tax rate of <?=100*$tax?>%</p>
 		<p class="normal-voice">Your tax amount is: $<?=$total - $order?></p>
 		<p class="normal-voice">Your grand total is: $<?=$total?> </p>
 	</output>
