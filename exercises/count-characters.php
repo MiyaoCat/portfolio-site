7c1f1d13c0cdcbcf3ea2586d695be900b857e112
john <jjmiyao@gmail.com>
2022-12-07 21:31:55 -0800

    Refactored code

--- exercises/count-characters.php
+++ exercises/count-characters.php
@@ -1,7 +1,31 @@
+<?php 
+	$string = "";
+	$length = 0;
+	$lengthWithSpaces = 0;
+	$word = 0;
+	$message = 0;
+	$errorMessage = "";
+
+	if ( isset($_POST["string"]) ) {
+		$string = $_POST["string"];
+
+		if ($string == "") {
+		 	$errorMessage = "Please type something in the text prompt. Otherwise, what's the point? You can't count to zero.";
+		} 
+
+		else {
+			$length = strlen($string) - substr_count($string, ' ');
+			$lengthWithSpaces = strlen($string);
+			$word = str_word_count($string, 0);
+
+			$message = "<output>You entered \"<span>$string</span>\". It is <span class='length'>$length</span> characters long without spaces</output>";
+		}
+	}
+
+?>
 
 <form method="POST">
 <h1 class="loud-voice">Character Count</h1>
-
  	
 	<div class="instructions">
 		<p class="regular-voice">Type in a string of text and I'll count how many characters you entered. </p>
@@ -11,53 +35,40 @@
 		type="text" 
 		name="string" 
 		value="" 
-		placeholder="Type whatever you want">
+		placeholder="Type whatever you want"
+	>
+	<p class="calm-voice warning"><?=$errorMessage?></p>
 
 	<button type="submit" name="enter">Submit</button>
  </form>
 
 
- 	
-<output>
-<?php 
-	//setup. Establish variables
-	$string = "";
-	$length = "";
-	$lengthWithSpaces = "";
+<?=$message?>
 
-	//If there's a form submission. Reassign values
-	if ( isset($_POST["string"]) ) {
-		 $string = $_POST["string"];
 
-		 if ($string == "") {
-		 	echo "<output> <p class='regular-voice'> Please type something in the text prompt. Otherwise, what's the point? You can't count to zero.</p></output>";
-		 } else {
-	//calculations
-	$length = strlen($string) - substr_count($string, ' ');
-	$lengthWithSpaces = strlen($string);
-	$word = str_word_count($string, 0);
-
-	//Return message
-	$message = "You entered \"<span>$string</span>\". It is <span class='length'>$length</span> characters long without spaces";
-
- ?>
-
-	<p class="regular-voice"><?=$message?></p><br>
-
-	<p class="regular-voice">Or it's <?=$lengthWithSpaces?> characters WITH spaces.</p>
-	<br>
-	<p class="regular-voice">Or it's <?=$word?> word(s).</p>
-
-<?php } ?>	
-<?php } ?>
-</output>
+	
 
 
 
 
 
 
-
+<div class="test" style="grid-row: 8; grid-column: 4 / 10;">
+	<?php
+$a = 10;
+echo 'Value of $a is :'.$a;
+echo '<br />After Pre-increment value of $a ( i.e. ++$a ) is: '.++$a;
+$a = 20;
+echo '<br />Value of $a is :'.$a;
+echo '<br />After Post-increment value of $a ( i.e. $a++ ) is: '.$a++;
+$a = 30;
+echo '<br />Value of $a is :'.$a;
+echo '<br />After Pre-decrement value of $a ( i.e. --$a ) is: '.--$a;
+$a = 40;
+echo '<br />Value of $a is :'.$a;
+echo '<br />After Post-decrement value of $a ( i.e. $a-- ) is: '.$a--;
+	?>
+</div>
 
 
 
