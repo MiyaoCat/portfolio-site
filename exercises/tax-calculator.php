b3f41378e8b1ace4623879968ca4ffaa9fbc596d
john <jjmiyao@gmail.com>
2022-12-14 01:34:32 -0800

    Retreived county selected tax

--- exercises/tax-calculator.php
+++ exercises/tax-calculator.php
@@ -1,9 +1,5 @@
 <?php  
-function isSelected($chosen, $current) {
-	if ($chosen == $current) {
-		return "selected";
-	}
-}
+
 
 $stateMessage = "";
 
@@ -22,32 +18,30 @@ $json = file_get_contents("data/exercises/tax-calculator.json");
 $statesData = json_decode($json, true);
 
 $stateMessage = "";
+$selectedStateAbbr = null;
+$selectedState = null;
+$countySelected = null;
+$countyTax = null;
 
+$state = null;
 // USER INPUT 
 $submitted = isset($_POST["submitted"]);
 var_dump($_POST);
 
 if ($submitted) {
 
-
 	if ( isset($_POST["state-selected"]) ) { 
+		$selectedStateAbbr = $_POST["state-selected"]; 
 
-		//variable to reuse state user selected
-		$stateSelected = $_POST["state-selected"];
-	}
-
-	if ($stateSelected == "") {
-			$StateMessage = "<p class='calm-voice warning'>You didn't select a state!</p>";
+		foreach($statesData as $state) {
+			if ($selectedStateAbbr == $state["abbr"]) {
+				$selectedState = $state;
+			}
+		}
 	}
 
 	if ( isset($_POST["county-selected"]) ) { 
-
-		//variable to resuse county user selected
 		$countySelected = $_POST["county-selected"];
-
-		if ($countySelected == "") {
-			$countyMessage = "<p class='calm-voice warning'>You didn't select a state!</p>";
-		}
 	}
 }	
 
@@ -59,22 +53,32 @@ function findStateTaxData($statesData, $stateId) {
 		}
 	}
 }
+ $stateData = findStateTaxData($statesData, $selectedStateAbbr);
+
+function isSelected($chosen, $current) {
+	if ($chosen == $current) {
+		return "selected";
+	}
+}
+
 
 ?>
 
 <h3 class="attention-voice"><a href="#">Reset</a></h3>
 <form method="POST">
 	<p class="normal-voice">Enter the price of your item.</p>	
+
 	<input 
 		type="number"
 		name="order-amount"
-		value=""
+		value="10"
 		step=".01"
 		placeholder="Enter a number"
 		autocomplete="off"
 	>
 
 	<p class="warning calm-voice"></p>
+
 
 	<label for="state-selected" class="normal-voice">Please select a state:</label>
 
@@ -85,49 +89,57 @@ function findStateTaxData($statesData, $stateId) {
 			foreach($statesData as $state) {
 				$id = $state["abbr"];		
 		?>
-			<option value="<?=$id?>" <?=isSelected($stateSelected, $id)?>><?=$id?>
-				
+			<option value="<?=$id?>" <?=isSelected($selectedStateAbbr, $id)?>>	<?=$id?>
 			</option>
 		<?php } ?>
 
-		<?php if($stateMessage) { ?>
-			<p class="warning calm-voice"><?=$stateMessage?></p>
+		<?php if($message) { ?>
+			<p class="warning calm-voice"><?=$message?></p>
 		<?php } ?>
 	</select>
 
-	<label for="county-selected" class="normal-voice">Please select a county:</label>
 
-	<select name="county-selected" id="county">
-		<option value="">--Select a State--</option>
+	<?php 
+		if ($selectedStateAbbr) {
+	?>
+		<label for="county-selected" class="normal-voice">Please select a county:</label>
 
-		<?php 
-			foreach($statesData as $state) {
-				$county = $state["counties"];
+		<select name="county-selected" id="county">
+			<option value="">--Select a County--</option>
 
-				
-					// foreach($county as $county) {
-		?>
-			<option value="<?=$county?>" <?=isSelected($countySelected, $id)?>><?=$id?>
-				
-			</option>
-					
+			<?php  
+				foreach($selectedState["counties"] as $county) {
+
+			?>	
+					<option value="<?=$county["name"]?>" <?=isSelected($countySelected, $county["name"])?>>
+
+						<?=$county["name"]?>
+
+					</option>
 			<?php } ?>
-	</select>
-		
+		</select>
+
+	<?php } ?>
 	<p class="warning calm-voice"></p>
 	
-
 	<button type="submit" name="submitted">Submit</button>
 </form>
 
+<?php 
+	if($submitted) { 
+		if($_POST["state-selected"] != null && $_POST["order-amount"] != null) {
+?>
 	<output>
-		<p class="normal-voice"><?=$_POST["state-selected"]?> has a tax rate of <?=$tax?>%</p>
-		<p class="normal-voice">Your tax amount is: $<?=total($taxData, $_POST)?></p>
-		<p class="normal-voice">Your grand total is: $<?=total($taxData, $_POST) + $order?> </p>
+		<p class="normal-voice">You selected <?=$countySelected?> county in <?=$selectedState["state"]?>.</p>
+		<p class="normal-voice">Your order price was $<?=$_POST["order-amount"]?>.</p>
+		<p class="normal-voice">State sales tax rate: <?=$selectedState["tax"]?>% </p>
+		<p class="normal-voice">County sales tax rate: % </p>
+		<p class="normal-voice">Your tax amount is: </p>
+		<p class="normal-voice">Your grand total is: </p>
 	</output>
+<?php } ?>
 
-
-
+<?php } ?>
 
 
 
