c6c3cb73e7db615fa7a0e04bbadb7f19132c5fec
john <jjmiyao@gmail.com>
2022-12-19 22:50:11 -0800

    cascade

--- exercises/password-validation.php
+++ exercises/password-validation.php
@@ -1,7 +1,7 @@
 <?php  
 	$fruit = "";
 	$number = "";
-	$createdPassword = $fruit . $number;
+
 	$submitted = isset($_POST["submitted"]);
 
 	if ($submitted) {
@@ -11,24 +11,52 @@
 		$passwordEnter = "";
 	}
 
+if($submitted) {
 
-function password($fruit, $number, $password) {
+	$successMessage = "";
 
-}
+	if ( isset($_POST["fruit"]) && isset($_POST["number"]) && isset($_POST["password"]) ) {
+		if ( $_POST["fruit"] != null  && $_POST["number"] != null ) {
+				if ($createdPassword == $_POST["password"]) {
+
+					 $successMessage = "You're in!";
+				}				
+		}
+	} 
+} 
+
+
+function successMessage($fruit, $number, $password) {
 	if($submitted) {
 
-		$successMessage = "";
+	$successMessage = "";
 
-		if ( isset($_POST["fruit"]) && isset($_POST["number"]) && isset($_POST["password"]) ) {
-			if ( $_POST["number"] != null  && $_POST["number"] != null ) {
-					if ($createdPassword == $_POST["password"]) {
+	if ( isset($fruit) && isset($number) && isset($password) ) {
+		if ( $fruit != null  && $number != null ) {
+				if ($fruit . $number == $password) {
 
-						 $successMessage = "You're in!";
-					}				
-			}
-		} 
+					 $successMessage = "You're in!";
+				}				
+		}
 	} 
+} 
+}
 
+function createdPassword($fruit, $number) {
+	return $fruit . $number;
+}
+
+function errorMessageFruit() {
+	if ( isset($_POST["fruit"]) && ($_POST["fruit"] == null) ) {
+		echo "Please enter a fruit";
+	}
+}
+
+function errorMessageNumber() {
+	if ( isset($_POST["number"]) && ($_POST["number"] == null) ) {
+		echo "Please enter a number";
+	}
+}
 ?>
 
 <form method="POST" class="password">
@@ -36,31 +64,34 @@ function password($fruit, $number, $password) {
 	<div class="password-create">
 		<label for="fruit">What is your favorite fruit?</label>
 		<input type="text" name="fruit" value="<?=$fruit?>">
-		
+		<p class="calm-voice warning"><?=errorMessageFruit()?></p>
+
 		<label for="number">What is your favorite number?</label>
 		<input type="number" name="number" value="<?=$number?>">
+		<p class="calm-voice warning"><?=errorMessageNumber()?></p>
 	</div>
 
 	<?php  
 		if ($submitted) {
-			if ( isset($_POST["fruit"]) && $_POST["fruit"] != null ) {
-				if ( isset($_POST["number"]) && $_POST["number"] != null ) {
+			if ( isset($_POST["fruit"]) && isset($_POST["number"]) ) {
+				if ( $_POST["fruit"] != null && $_POST["number"] != null ) {
 	?>
-					<div class="password-output">
-						<p class="normal-voice">You entered:</p>
-							<span class="alert-voice"><?php  $createdPassword = $fruit . $number; ?>		
-						<?=$createdPassword?></span>
-						
-						<p class="normal-voice">This will be your password.</p>
-					</div>
+			<div class="password-output">
+				<p class="normal-voice">You entered:</p>
+					<span class="alert-voice">
 
-					<password-enter>
-						<label for="password">Please enter your password:</label>
-						
-						<input type="text" name="password" value="">
-					</password-enter>
+				<?=createdPassword($_POST["fruit"], $_POST["number"])?></span>
+				
+				<p class="normal-voice">This will be your password.</p>
+			</div>
+
+			<password-enter>
+				<label for="password">Please enter your password:</label>
+				
+				<input type="text" name="password" value="">
+			</password-enter>
+				<?php } ?>
 			<?php } ?>
-		<?php } ?>
 	<?php } ?>
 
 
