36ee6ee4558b3259abf6cbadd3341ad517137e24
john <jjmiyao@gmail.com>
2022-12-20 15:13:20 -0800

    Contact form fix

--- exercises/scratch.php
+++ exercises/scratch.php
@@ -290,7 +290,7 @@
 
 
 
-
+<!-- -- -- -- -- -- TAX CALCULATOR -- -- -- -- -- -- -->
 
 
 /// BEFORE RETURN ARRAY OUTPUT
@@ -553,6 +553,77 @@ function isSelected($chosen, $current) {
 	</output>
 
 
+
+
+<!-- -- -- -- -- -- PASSWORD -- -- -- -- -- -- -->
+
+<?php  
+	$fruit = "";
+	$number = "";
+	$password = $fruit . $number;
+	$submitted = isset($_POST["submitted"]);
+
+?>
+
+<form method="POST" class="password">
+
+	<?php  
+		// $hide = "";
+		// $unHide = "";
+
+		// if ( isset($_POST["fruit"]) AND isset($_POST["number"]) ) {
+		// 	$hide = "style='display:none;'"; 
+		// }
+
+		// elseif ( isset($_POST["password"]) ) {
+		// 	if ($_POST["password"] !== $_POST["createdPassword"]) {
+		// 		$unHide = "style='display:flex'";
+		// 	}
+		// }	
+
+	?>
+		<div class="password-create">
+			<label for="fruit">What is your favorite fruit?</label>
+			<input type="text" name="fruit" value="<?=$fruit?>">
+			
+			<label for="number">What is your favorite number?</label>
+			<input type="number" name="number" value="<?=$number?>">
+		</div>
+
+
+	<?php  
+	if ($submitted) {
+		$fruit = $_POST["fruit"];
+		$number = $_POST["number"];
+		$password = $fruit . $number;
+		$passwordEnter = "";
+
+		if ( isset($_POST["password"]) ) {
+			if ($_POST["password"] == $password) {
+				echo "correct";
+			}
+		};
+
+	?>
+		<output>
+			<p class="normal-voice">You entered:</p>
+			<?php  $password = $fruit . $number; ?>		
+			<?=$password?>	
+			
+			<p class="normal-voice">This will be your password</p>
+		</output>
+
+		<form-wrap>
+			<label for="password">Please enter your password</label>
+			<input type="text" name="password" value="<?=$passwordEnter?>">
+			<input class="hidden" type="text" name="createdPassword" value="<?=$password?>">
+		</form-wrap>
+
+
+	<?php } ?>
+	<button type="submit" name="submitted">Submit</button>
+
+</form>
 
 
 
--- modules/email-form/email-form.php
+++ modules/email-form/email-form.php
@@ -10,44 +10,46 @@
 
 		$body = "";
 
-		$body = "Email: " . $email . "\r\n";
-		$body = "Subject: " . $subject . "\r\n";
-		$body = "Name: " . $name . "\r\n";
+		$email = "Email: " . $email . "\r\n";
+		$subject = "Subject: " . $subject . "\r\n";
+		$name = "Name: " . $name . "\r\n";
 		$body = "Message: " . $message . "\r\n";
 
 
-		mail($to, $subject, $body);
+		mail($to, $subject, $email, $body);
 
 	}
 ?>
-<form method="POST">
-	
-	<label for="inputName">Name</label>
-	<input type="text" name="inputName">
 
-	<label for="inputEmail">* Email</label>
-	<input type="email" name="inputEmail" required>
+<div class="form" <?php if(isset($_POST['submitted'])){ echo "style='display:none'"; } ?>>   
+	<form method="POST">
+		
+		<label for="inputName">Name</label>
+		<input type="text" name="inputName">
 
-	<label for="inputSubject">Subject</label>
-	<input type="text" name="inputSubject">
+		<label for="inputEmail">* Email</label>
+		<input type="email" name="inputEmail" required>
 
-	<label for="inputMessage">* Message</label>
-	<textarea rows="8" columns="10" name="inputMessage" placeholder="Hey John, I love your work. Let's collab!" required></textarea>
+		<label for="inputSubject">Subject</label>
+		<input type="text" name="inputSubject">
 
-	<button class="normal-voice contact-form" type="submit" name="submitted">Send Message</button>
+		<label for="inputMessage">* Message</label>
+		<textarea rows="8" columns="10" name="inputMessage" placeholder="Hey John, I love your work. Let's collab!" required></textarea>
 
-</form>
+		<button class="normal-voice contact-form" type="submit" name="submitted">Send Message</button>
 
+	</form>
+</div>
 
 <?php  
 	if ( isset($_POST["submitted"]) ) {
 
 		if ( isset($email) ) {
-		if ( isset($message) && $message > "0") {	
+			if ( isset($message) && $message > "0") {	
 ?>
 			<p class="normal-voice">Thank you! Your message has been received and I will get back to you ASAP!</p>
 
-		<?php } ?>
+			<?php } ?>
 		
 		<?php } ?>
 
