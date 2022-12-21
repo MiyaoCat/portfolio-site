1e07dc946e578ba1cb8be8c26149f4b1ae1741a5
john <jjmiyao@gmail.com>
2022-12-20 23:32:42 -0800

    Styling form page headers

--- exercises/count-characters.php
+++ exercises/count-characters.php
@@ -29,9 +29,6 @@
 
 ?>
 
-<form method="POST">
-<h1 class="loud-voice">Character Count</h1>
- 	
 <form method="POST">	
 	<div class="instructions">
 		<p class="regular-voice">Type in a string of text and I'll count how many characters you entered. </p>
