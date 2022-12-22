e9f2c94c2e930d7b02654893e43673a8f5412385
john <jjmiyao@gmail.com>
2022-12-21 19:44:19 -0800

    Fixed form grid

--- modules/email-form/email-form.php
+++ modules/email-form/email-form.php
@@ -11,7 +11,7 @@
 		$body = "";
 
 		$body = "Email: " . $email . "\r\n" . 
-			"Subject: " . $subject . "\r\n" . 
+			// "Subject: " . $subject . "\r\n" . 
 			"Name: " . $name . "\r\n" . 
 			"Message: " . $message . "\r\n";
 
@@ -21,25 +21,33 @@
 	}
 ?>
 
-<div class="contact" <?php if(isset($_POST['submitted'])){ echo "style='display:none'"; } ?>>    
-	<form method="POST">
-		
+
+<form class="contact" method="POST" <?php if(isset($_POST['submitted'])){ echo "style='display:none'"; } ?>>
+	
+	<div class="name">
 		<label for="inputName">Name</label>
 		<input type="text" name="inputName">
+	</div>
 
+	<div class="email">
 		<label for="inputEmail">* Email</label>
 		<input type="email" name="inputEmail" required>
+	</div>
 
+	<div class="subject">
 		<label for="inputSubject">Subject</label>
 		<input type="text" name="inputSubject">
+	</div>
 
+	<div class="message">
 		<label for="inputMessage">* Message</label>
-		<textarea rows="8" columns="10" name="inputMessage" placeholder="Hey John, I love your work. Let's collab!" required></textarea>
+		<textarea rows="6" columns="10" name="inputMessage" placeholder="Hey John, I love your work. Let's collab!" required></textarea>
+	</div>
 
-		<button class="normal-voice contact-form" type="submit" name="submitted">Send Message</button>
+	<button class="normal-voice contact-form" type="submit" name="submitted">Send Message</button>
 
-	</form>
-</div>
+</form>
+
 
 <?php  
 	if ( isset($_POST["submitted"]) ) {
