ad220cd2c1b74535137dcb9b1d12c42264deb7a5
john <jjmiyao@gmail.com>
2023-01-18 16:33:45 -0800

    Added error message to password

--- exercises/password-validation.php
+++ exercises/password-validation.php
@@ -1,6 +1,8 @@
 <?php  
 	$fruit = "";
 	$number = "";
+	$postFruit = "";
+	$postNumber = "";
 
 	$submitted = isset($_POST["submitted"]);
 
@@ -14,37 +16,41 @@
 if($submitted) {
 
 	$successMessage = "";
+	$passwordErrorMessage = "";
 
 	if ( isset($_POST["fruit"]) && isset($_POST["number"]) && isset($_POST["password"]) ) {
 		if ( $_POST["fruit"] != null  && $_POST["number"] != null ) {
 				if ($createdPassword == $_POST["password"]) {
-
-					 $successMessage = "You're in!";
-				}				
+					 $successMessage = "You're in the matrix!";
+				}else {
+					$passwordErrorMessage = "ERRR. Wrong!";
+				}			
 		}
 	} 
-} 
 
+} 
 
 function successMessage($fruit, $number, $password) {
 	if($submitted) {
 
-	$successMessage = "";
+		$successMessage = "";
 
-	if ( isset($fruit) && isset($number) && isset($password) ) {
-		if ( $fruit != null  && $number != null ) {
-				if ($fruit . $number == $password) {
+		if ( isset($fruit) && isset($number) && isset($password) ) {
+			if ( $fruit != null  && $number != null ) {
+					if ($fruit . $number == $password) {
 
-					 $successMessage = "You're in!";
-				}				
-		}
+						 $successMessage = "You're in!";
+					}				
+			}
+		} 
 	} 
-} 
 }
 
 function createdPassword($fruit, $number) {
 	return $fruit . $number;
 }
+
+$createdPassword = createdPassword($fruit, $number);
 
 function errorMessageFruit() {
 	if ( isset($_POST["fruit"]) && ($_POST["fruit"] == null) ) {
@@ -57,6 +63,8 @@ function errorMessageNumber() {
 		echo "Please enter a number";
 	}
 }
+
+
 ?>
 
 <form method="POST" class="password">
@@ -78,9 +86,8 @@ function errorMessageNumber() {
 	?>
 			<div class="password-output">
 				<p class="normal-voice">You entered:</p>
-					<span class="alert-voice">
-
-				<?=createdPassword($_POST["fruit"], $_POST["number"])?></span>
+					
+					<span class="alert-voice"><?=$createdPassword?></span>
 				
 				<p class="normal-voice">This will be your password.</p>
 			</div>
@@ -89,12 +96,15 @@ function errorMessageNumber() {
 				<label for="password">Please enter your password:</label>
 				
 				<input type="text" name="password" value="">
+
+				<p class="calm-voice warning"><?=$passwordErrorMessage?></p>
+
 			</password-enter>
 				<?php } ?>
 			<?php } ?>
 	<?php } ?>
 
-
+	
 	<button type="submit" name="submitted">Submit</button>
 </form>
 
