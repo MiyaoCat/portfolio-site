e3360078b1fcbdc2d310de6aafeb84adfdedd7ad
john <jjmiyao@gmail.com>
2023-02-23 23:07:29 -0800

    Styled menu item-cards

--- projects/menu-app2/menu-items.js
+++ projects/menu-app2/menu-items.js
@@ -4,28 +4,28 @@ var menuItems = [
 		name: "Ramen",
 		description: "Yummy in your tummy",
 		price: 10,
-		image: "./images/ramen.jpeg",
+		image: "https://source.unsplash.com/random/?ramen,food"
 	},
 	{
 		slug: "salmon",
 		name: "Salmon",
 		description: "Delicious fish",
 		price: 8,
-		image: "./images/salmon.jpeg",
+		image: "https://source.unsplash.com/random/?food,vegan,sandwich,bread"
 	},
 	{
 		slug: "chicken-karaage",
 		name: "Chicken Karaage",
 		description: "Cheeky chickies",
 		price: 10,
-		image: "./images/saba.jpeg",
+		image: "https://source.unsplash.com/random/?food,salmon"
 	},
 	{
 		slug: "udon",
 		name: "Udon",
 		description: "Thicc noods",
 		price: 10,
-		image: "./images/saba.jpeg",
+		image: "https://source.unsplash.com/random/?fried-chicken,food"
 	}
 ]
 
