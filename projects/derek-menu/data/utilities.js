1b61ea9f67fc58154af021630a5d22c03b8ebba7
john <jjmiyao@gmail.com>
2023-02-28 23:01:03 -0800

    Begin shoe layout project. Setting up HTML

--- projects/derek-menu/data/utilities.js
+++
@@ -1,9 +0,0 @@
-function getData(key) {
-	return JSON.parse(localStorage.getItem(key));
-}
-
-function setData(key, value) {
-	return localStorage.setItem(key, JSON.stringify(value));
-}
-
-export { getData, setData };
--- projects/derek-menu/inventory.js
+++
@@ -1,7 +0,0 @@
-function addItem(item) {
-	var menu = getData("menu");
-	menu.push(item); // add it to the menu
-	setData("menu", menu); // save
-}
-
-export { addItem };
