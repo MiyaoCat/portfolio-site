fd228abb6a4caff6b971a9c9b96cc79339c888e6
john <jjmiyao@gmail.com>
2023-04-03 17:47:31 -0700

    Styled shoe-detail. Created shoe-detail background svg

--- projects/shoe-animation-layout/index.php
+++ projects/shoe-animation-layout/index.php
@@ -187,37 +187,36 @@
 					<li> <?php include('./assets/dot-red.svg'); ?> </li>
 					<li> <?php include('./assets/dot-grey.svg'); ?> </li>
 				</ul>
-
 			</div>
 			
 			<div class='shoe-detail'>
-				<ol class='size'>
-					<li><p class='alert-voice'>8</p></li>
-
-					<li><p class='alert-voice'>9</p></li>
-
-					<li>
-						<p class='alert-voice'>10</p>
-						<p class='calm-voice'>Size</p>
-					</li>
-
-					<li><p class='alert-voice'>11</p></li>
-					<li><p class='alert-voice'>12</p></li>
-				</ol>
-			
-				<p class='loud-voice'>Nike Blazer Low</p>
-			
-				<div class='review'>
-					<p class='normal-voice'>Review 1k+</p>
-
-					<div class='rating'>
-						<ul>
-							<li><?php include('./assets/star-yellow.svg'); ?></li>
-							<li><?php include('./assets/star-yellow.svg'); ?></li>
-							<li><?php include('./assets/star-yellow.svg'); ?></li>
-							<li><?php include('./assets/star-yellow.svg'); ?></li>
-							<li><?php include('./assets/star-grey.svg'); ?></li>
-						</ul>
+				<div class="shoe-detail-wrap">
+					<ol class='size'>
+						<li><p class='alert-voice'>8</p></li>			
+						<li><p class='alert-voice'>9</p></li>
+						<li>
+							<p class='alert-voice'>10</p>
+							<p class='calm-voice'>Size</p>
+						</li>
+					
+						<li><p class='alert-voice'>11</p></li>
+						<li><p class='alert-voice'>12</p></li>
+					</ol>
+								
+					<p class='shoe-name loud-voice'>Nike Blazer Low</p>
+								
+					<div class='review'>
+						<p class='normal-voice'>Review 1k+</p>
+					
+						<div class='rating'>
+							<ul>
+								<li><?php include('./assets/star-yellow.svg'); ?></li>
+								<li><?php include('./assets/star-yellow.svg'); ?></li>
+								<li><?php include('./assets/star-yellow.svg'); ?></li>
+								<li><?php include('./assets/star-yellow.svg'); ?></li>
+								<li><?php include('./assets/star-grey.svg'); ?></li>
+							</ul>
+						</div>
 					</div>
 				</div>
 
