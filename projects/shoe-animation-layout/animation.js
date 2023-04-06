e439a3b46dcc937c6fcd31a1f33d4be5f6610894
john <jjmiyao@gmail.com>
2023-04-05 22:34:04 -0700

    Added gsap to header

--- projects/shoe-animation-layout/animation.js
+++ projects/shoe-animation-layout/animation.js
@@ -1,1 +1,19 @@
-console.log("hello world");
+const headerTimeline = gsap.timeline();
+
+headerTimeline 
+	.from('.logo', {
+		y: -200,
+		duration: 1,
+	})
+	.from('nav', {
+		y: -200,
+		duration: 1,
+	})
+	.from('.model', {
+		y: -200,
+		duration: 1,
+	})
+	.from('.bag-icon', {
+		y: -200,
+		duration: 1,
+	})		
--- projects/shoe-animation-layout/index.php
+++ projects/shoe-animation-layout/index.php
@@ -228,7 +228,7 @@
 		</inner-column>
 
 	</main>	
-
+<script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/3.11.5/gsap.min.js"></script>
 <script src='animation.js'></script>			
 </body>
 
