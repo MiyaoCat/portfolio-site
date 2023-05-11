280959cfd10e14f7948cb8dedbc78b98ecc1553d
john <jjmiyao@gmail.com>
2023-05-10 22:07:06 -0700

    Added self-checkout form

---
+++ form-exercises/js-forms/self-checkout.js
@@ -0,0 +1,218 @@
+<form class="self-checkout" method="POST">
+
+	<div class="instructions">
+		<p class="normal-voice">Let's see how much you'll owe if you purchase up to 4 items in the state of Washington.</p>
+	</div>
+
+
+	<div class="items-wrap">
+		<div class="item one">
+			<h3 class="attention-voice">Item 1</h3>
+		 	
+		 	<div class="item-wrap">
+		 		<quantity>	
+				 	<p class="calm-voice">Quantity</p>
+					<input 
+						type="number" 
+						name="quantity" 
+						value="<?=$quantity?>"
+						min="0"
+						placeholder="0"
+						label="write your number"
+					>
+		
+					<p class="calm-voice warning"></p>		
+						
+				</quantity>		
+		
+				<price>
+					<p class="calm-voice">Price</p>	
+					<input 
+						type="number" 
+						name="price" 
+						value="<?=$price?>"
+						step= '0.01'
+						min='0'
+						placeholder="0.00">
+
+					<p class="calm-voice warning"></p>
+
+				</price>
+		 	</div>
+		</div>
+		
+		<div class="item two">
+			<h3 class="attention-voice">Item 2</h3>
+		
+		 	<div class="item-wrap">
+		 		<quantity>	
+				 	<p class="calm-voice">Quantity</p>
+					<input 
+						type="number" 
+						name="quantity2" 
+						value="<?=$quantity2?>"
+						min="0"
+						placeholder="0"
+						label="write your number">
+
+					<p class="calm-voice warning"></p>
+						
+				</quantity>		
+		
+				<price>
+					<p class="calm-voice">Price</p>	
+					<input 
+						type="number" 
+						name="price2" 
+						value="<?=$price2?>"
+						step= '0.01'
+						min='0'
+						placeholder="0.00">
+		
+					<p class="calm-voice warning"></p>
+						
+				</price>
+		 	</div>
+		
+		</div>
+		
+		<div class="item three">
+			<h3 class="attention-voice">Item 3</h3>
+		
+		 	<div class="item-wrap">
+		 		<quantity>	
+				 	<p class="calm-voice">Quantity</p>
+					<input 
+						type="number" 
+						name="quantity3" 
+						value="<?=$quantity3?>"
+						min="0"
+						placeholder="0"
+						label="write your number">
+
+						<p class="calm-voice warning"></p>
+						
+				</quantity>		
+		
+				<price>
+					<p class="calm-voice">Price</p>	
+					<input 
+						type="number" 
+						name="price3" 
+						value="<?=$price3?>"
+						step= '0.01'
+						min='0'
+						placeholder="0.00">		
+					
+					<p class="calm-voice warning"></p>						
+				</price>
+		 	</div>
+		</div>
+		
+		<div class="item four">
+			<h3 class="attention-voice">Item 4</h3>
+			
+			<div class="item-wrap">	
+				<quantity>	
+				 	<p class="calm-voice">Quantity</p>
+					<input 
+						type="number" 
+						name="quantity4" 
+						value="<?=$quantity4?>"
+						min="0"
+						placeholder="0"
+						label="write your number">
+									
+					<p class="calm-voice warning"></p>						
+				</quantity>		
+				
+				<price>
+					<p class="calm-voice">Price</p>	
+					<input 
+						type="number" 
+						name="price4" 
+						value="<?=$price4?>"
+						step= '0.01'
+						min='0'
+						placeholder="0.00">				
+					
+					<p class="calm-voice warning"></p>					
+				</price>
+			</div>
+		</div>
+	</div>
+
+	<tax>
+		<p class="normal-voice">The Washington State sales tax rate is %</p>
+	</tax>
+
+	<button type="submit" name="submitted">Submit</button>
+
+</form>
+
+<output>
+	<sub-total>
+		<p class="normal-voice">Your Sub-total is: <b>$</b></p>		
+	</sub-total>
+
+	<tax-total>
+		<p class="normal-voice">You total tax amount is: <b></b></p>
+	</tax-total>
+
+	<grand-total>
+		<p class="normal-voice">Your grand total is: <b></b></p>	
+	</grand-total>
+</output>
+
+<script>
+var $selfCheckout = document.querySelector("form");
+
+var $quantity1 = document.querySelector("[name='quantity']");
+var $price1 = document.querySelector("[name='price']");
+
+var $quantity2 = document.querySelector("[name='quantity2']");
+var $price2 = document.querySelector("[name='price2']");
+
+var $quantity3 = document.querySelector("[name='quantity3']");
+var $price3 = document.querySelector("[name='price3']");
+
+var $quantity4 = document.querySelector("[name='quantity4']");
+var $price4 = document.querySelector("[name='price4']");
+
+$selfCheckout.addEventListener('submit', function(myEvent) {
+	myEvent.preventDefault();
+	console.log("button pushed");
+
+	let quantity1 = $quantity1.value;
+	let price1 = $price1.value;
+
+	let quantity2 = $quantity2.value;
+	let price2 = $price2.value;
+
+	let quantity3 = $quantity3.value;
+	let price3 = $price3.value;
+
+	let quantity4 = $quantity4.value;
+	let price4 = $price4.value;
+
+	let subTotal = (quantity1 * price1) + (quantity2 * price2) + (quantity3 * price3) + (quantity4 * price4);
+	const subTotalFormatted = subTotal.toLocaleString('en-US');
+
+	const taxRate = .1025;
+
+	const taxTotal = (subTotal * taxRate).toFixed(2).toLocaleString('en-US');
+	const taxTotalFormatted = taxTotal.replace(/\B(?=(\d{3})+(?!\d))/g, ',');
+
+	const grandTotal = parseFloat(subTotal) + parseFloat(taxTotal);
+	const grandTotalFormatted = grandTotal.toLocaleString('en-US');
+
+	const $output = document.createElement("output");
+	document.body.appendChild($output);
+
+	$output.innerHTML = 
+		`<p class="normal-voice">Sub-total: $${subTotalFormatted}</p> 
+		 <p class="normal-voice">Tax total: $${taxTotalFormatted}</p> 
+		 <p class="normal-voice">Grand total: $${grandTotalFormatted}</p> 
+		`;
+});
+</script>
