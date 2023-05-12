d1e61616407f7c8c07b885333a8eb4820ca52e64
john <jjmiyao@gmail.com>
2023-05-11 21:13:20 -0700

    Styling radio button

--- modules/radio-button/radio-button.php
+++ modules/radio-button/radio-button.php
@@ -1,5 +1,5 @@
 <?php
-	$languageType = "php";
+	$languageType = "javascript";
 
 	function isChecked($type, $name) {
 		if ($type == $name) {
@@ -31,6 +31,7 @@
 
 		$page = "v-exercise";
 	};
+
 ?>
 
 <div id="languages">
@@ -47,41 +48,3 @@
 
 		<button type="submit" name="submitted">Submit</button>
 	</form>
-</div>
-
-
-<?php 
-	foreach ($exercises as $exercise) { 
-		if ($exercise["type"] !== "generic-text" and $exercise["type"] !== "language") {
-			$type = $exercise["type"];
-			$header = $exercise["header"];
-?>
-			<form-exercise class="<?=$type?>">
-				<h2 class="attention-voice"><?=$header?></h2>
-				
-				<ol class="exercises">
-					<?php foreach ($exercise["forms"] as $form) {
-						$slug = $form["slug"];
-					?>
-						<li>
-							<a href="?page=<?=$page?>&slug=<?=$slug?>">
-								<h3 class="normal-voice">
-									<?=$form["title1"]?> <?=$form["title2"]?>
-								</h3>
-							</a>
-						</li>	
-					<?php } ?>	
-				</ol>
-
-			</form-exercise>
-		<?php } 
-			elseif ($exercise["type"] == "generic-text") {
-
-		?>
-			<section class="generic-text"><?=$exercise["content"]?></section>
-			<?php } ?>
-	<?php } ?>
-
-
-
-
