3a0fadca70e6483cb818bff20192bfde0971786c
jmiyao <jjmiyao@gmail.com>
2023-05-13 23:11:50 -0700

    Fixed file path for vue form

--- partials/footer/footer.php
+++ partials/footer/footer.php
@@ -12,47 +12,10 @@
 	<a href="?page=exercises">Exercises</a>
 	</footer>
 
-<!-- <script src='javascript/exercises.js'></script>	 -->
 <script src="https://cdn.jsdelivr.net/npm/vue@2.7.14/dist/vue.js"></script>
-<!-- <script src="./v-exercises/v-simple-interest/script.js"></script> -->
+<!-- <script src="./../form-exercises/vue-forms/v-count-characters/script.js"></script> -->
 </body>
 
 <script>
-let $password = document.querySelector('form');
-
-let $fruit = document.querySelector("[name='fruit']");
-let $number = document.querySelector("[name='number']");
-
-let $output = document.querySelector("output");
-$output.hidden = true;
-
-let $passwordOutput = document.querySelector(".password-output");
-$passwordOutput.hidden = true;
-
-let $passwordEnter = document.querySelector("password-enter");
-$passwordEnter.hidden = true;
-
-$password.addEventListener('submit', function(event) {
-	event.preventDefault();
-
-	let fruitInput = $fruit.value;
-	let numberInput = $number.value;
-	let passwordInput = fruitInput + numberInput;	
-
-	$output.hidden = false;
-	if (!fruitInput) {
-		let $warning = document.querySelector('.calm-voice.warning');
-
-		$warning.innerHTML = 'No fruit entered';
-	} else {
-		$passwordOutput.hidden = false;
-
-		$passwordOutput.innerHTML = `<p class="normal-voice">You entered:</p> <span class="alert-voice">${passwordInput}</span> <p class="normal-voice">This will be your password.</p>`;
-
-		$passwordEnter.hidden = false;
-
-		}
-		
-});
 </script>
 </html>
