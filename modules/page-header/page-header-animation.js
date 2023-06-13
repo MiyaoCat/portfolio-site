53afed020396f51963020b98374cf694c0f73dac
john <jjmiyao@gmail.com>
2023-06-12 17:20:00 -0700

    Refactored page-header code

--- modules/page-header/page-header-animation.js
+++ modules/page-header/page-header-animation.js
@@ -1,6 +1,8 @@
 var titleOneTimeline = gsap.timeline();
 var titleTwoTimeline = gsap.timeline();
 
+var genericText = gsap.timeline();
+
 titleOneTimeline
 	.from('.title1', {
 		x: 400,
@@ -14,3 +16,4 @@ titleTwoTimeline
 		duration: 2,
 		ease: "elastic.out(1.5, .5)"
 	}, "<50%")
+
