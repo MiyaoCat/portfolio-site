82988a8f5d7157f0048484cd50fd922f8bcd1727
john <jjmiyao@gmail.com>
2023-06-14 17:07:32 -0700

    Added tilt effect to title1

--- modules/page-header/page-header-animation.js
+++ modules/page-header/page-header-animation.js
@@ -1,19 +1,53 @@
+// var titleOneTimeline = gsap.timeline();
+// var titleTwoTimeline = gsap.timeline();
+
+// var genericText = gsap.timeline();
+
+// titleOneTimeline
+// 	.from('.title1', {
+// 		x: 400,
+// 		duration: 2,
+// 		ease: "elastic.out(1.5, .5)"
+// 	})
+
+// titleTwoTimeline
+// 	.from('.title2', {
+// 		x: -600,
+// 		duration: 2,
+// 		ease: "elastic.out(1.5, .5)"
+// 	}, "<50%")
+
+// gsap.to('.title1', {
+//   rotation: -7,
+//   y: 20,
+//   duration: 1,
+//   ease: "bounce.out"
+// });
+
 var titleOneTimeline = gsap.timeline();
 var titleTwoTimeline = gsap.timeline();
 
 var genericText = gsap.timeline();
 
 titleOneTimeline
-	.from('.title1', {
-		x: 400,
-		duration: 2,
-		ease: "elastic.out(1.5, .5)"
-	})
+  .from('.title1', {
+    x: 400,
+    duration: 2,
+    ease: "elastic.out(1.5, .5)",
+    onComplete: function() {
+      // Start the last animation after a 2-second delay
+      gsap.to('.title1', {
+        rotation: -7,
+        y: 20,
+        duration: 1,
+        ease: "bounce.out"
+      });
+    }
+  });
 
 titleTwoTimeline
-	.from('.title2', {
-		x: -600,
-		duration: 2,
-		ease: "elastic.out(1.5, .5)"
-	}, "<50%")
-
+  .from('.title2', {
+    x: -600,
+    duration: 2,
+    ease: "elastic.out(1.5, .5)"
+  }, "<50%");
