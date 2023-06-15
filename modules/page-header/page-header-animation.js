30ee3222cc8b0faa8fcd6b650d1e278e4fa824d3
john <jjmiyao@gmail.com>
2023-06-15 10:27:26 -0700

    Removed tilt and hover animations

--- modules/page-header/page-header-animation.js
+++ modules/page-header/page-header-animation.js
@@ -19,12 +19,12 @@ titleOneTimeline
 	        yValue = 20; 
       }
       //title1 tilts down
-      gsap.to('.title1', {
-			rotation: -9,
-			y: yValue,
-			duration: 1,
-			ease: "bounce.out"
-      });
+      // gsap.to('.title1', {
+		// 	rotation: -9,
+		// 	y: yValue,
+		// 	duration: 1,
+		// 	ease: "bounce.out"
+      // });
     }
   });
 
@@ -37,46 +37,46 @@ titleTwoTimeline
 
 
 //Listen for mousover on title1 to untilt it and make horizontal again
-document.querySelector('.title1').addEventListener('mouseover', function() {
-  gsap.to(this, {
-    rotation: 0,
-    y: 0,
-    duration: 0.3,
-    ease: "power1.inOut"
-  });
-});
+// document.querySelector('.title1').addEventListener('mouseover', function() {
+//   gsap.to(this, {
+//     rotation: 0,
+//     y: 0,
+//     duration: 0.3,
+//     ease: "power1.inOut"
+//   });
+// });
 
-//Listen for mouseout event on title1 and tilt back to original
-document.querySelector('.title1').addEventListener('mouseout', function() {
-	var screenWidth = window.innerWidth || document.documentElement.clientWidth;
-	   var yValue = -18;
+// //Listen for mouseout event on title1 and tilt back to original
+// document.querySelector('.title1').addEventListener('mouseout', function() {
+// 	var screenWidth = window.innerWidth || document.documentElement.clientWidth;
+// 	   var yValue = -18;
 
-	   if (screenWidth >= 700) {
-	   	yValue = 20;
-	   }
-	//tilt back to original place
-	gsap.to(this, {
-		rotation: -9,
-		y: yValue,
-		duration: .4,
-		ease: "elastic.out(1.2, .6)"
-	});
-});
+// 	   if (screenWidth >= 700) {
+// 	   	yValue = 20;
+// 	   }
+// 	//tilt back to original place
+// 	gsap.to(this, {
+// 		rotation: -9,
+// 		y: yValue,
+// 		duration: .4,
+// 		ease: "elastic.out(1.2, .6)"
+// 	});
+// });
 
-//Listen for screen width to ensure title stays in place 
-function updateTitle1Position() {
-	var screenWidth = window.innerWidth || document.documentElement.clientWidth;
+// //Listen for screen width to ensure title stays in place 
+// function updateTitle1Position() {
+// 	var screenWidth = window.innerWidth || document.documentElement.clientWidth;
 
-	// Adjust the y value based on the screen width
-	if (screenWidth >= 700) {
-		$title1.style.transform = 'translateY(20px)';
-	} else {
-		$title1.style.transform = 'translateY(' + initialY + 'px)';
-	}
-}
+// 	// Adjust the y value based on the screen width
+// 	if (screenWidth >= 700) {
+// 		$title1.style.transform = 'translateY(20px)';
+// 	} else {
+// 		$title1.style.transform = 'translateY(' + initialY + 'px)';
+// 	}
+// }
 
-// Update the position initially
-updateTitle1Position();
+// // Update the position initially
+// updateTitle1Position();
 
-// Listen for the window resize event
-window.addEventListener('resize', updateTitle1Position);
+// // Listen for the window resize event
+// window.addEventListener('resize', updateTitle1Position);
