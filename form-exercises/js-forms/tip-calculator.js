b64cb8c19044dfa1eaefe500141e42f0db047a46
john <jjmiyao@gmail.com>
2023-07-12 15:21:15 -0700

    Added form validation

--- form-exercises/js-forms/tip-calculator.js
+++ form-exercises/js-forms/tip-calculator.js
@@ -1,32 +1,53 @@
-<form method="POST">
+<form method="POST" id="tip-calc">
 	<div class="instructions">
 		<p class="normal-voice">Let us know the amount of your order and the tip percentage you'd like to leave. We'll provide the grand total.</p>
 	</div>
 
 	<div class="input-one">
 		<label for="subtotal">What is the order amount?</label>
-		<input type="number" name="subtotal" value="" step="0.01" min=0 required>
+		<input type="number" id="subtotal" value="" step="0.01" min=0>
+		<p class="calm-voice warning"></p>
 	</div>
 	
 	<div class="tip-select">
 		<p class="normal-voice">How much would you like to tip?</p>
-		<div class="ten">
-			<label for="ten-percent">10 Percent</label>
-			<input type="radio" name="tip" class="percent" value=0.10>
-		</div>
 
-		<div class="fifteen">
-			<label for="fifteen-percent">15 Percent</label>
-			<input type="radio" name="tip" class="percent" value=0.15>
-		</div>
-
-		<div class="twenty">
-			<label for="twenty-percent">20 Percent</label>
-			<input type="radio" name="tip" class="percent" value=.20>
+		<div class="tip-options">
+			<div class="ten">
+				<label for="ten-percent">10 Percent</label>
+				<input 
+					type="radio" 
+					id="ten-percent"
+					name="tip" 
+					class="percent" 
+					value=0.10
+				>
+			</div>
+			
+			<div class="fifteen">
+				<label for="fifteen-percent">15 Percent</label>
+				<input 
+					type="radio" 
+					id="fifteen-percent"
+					name="tip" 
+					class="percent" 
+					value=0.15
+				>
+			</div>
+			
+			<div class="twenty">
+				<label for="twenty-percent">20 Percent</label>
+				<input 
+					type="radio" 
+					id="twenty-percent"
+					name="tip" 
+					class="percent" 
+					value=.20
+				>
+			</div>
 		</div>
 
 		<p class="calm-voice warning"></p>
-
 	</div>
 	
 	<button type="submit" name="submitted">
@@ -36,30 +57,6 @@
 </form>
 
 <output>
-	<table>
-		<tr>
-			<td>
-				<p class="normal-voice">Your subtotal is:</p>
-			</td>
-			<td><p><b></b></p></td>
-		</tr>
-
-		<tr>
-			<td>
-				<p class="normal-voice">Your tip amount is:</p>
-			</td>
-			<td><p><b></b></p></td>
-		</tr>
-		
-		<tr>
-			<td>
-				<p class="normal-voice">Your grand total is:</p>
-			</td>
-			<td>
-				<p><b></b></p>
-			</td>
-		</tr>
-	</table>
 </output>
 
 <div class='return'>
@@ -67,11 +64,13 @@
 </div>
 
 <script>
-var $form = document.querySelector('form');
-var $subTotal = document.querySelector("[name='subtotal']");
-var $tip = document.querySelector("input[name='tip']");
+let $form = document.getElementById("tip-calc");
+let $subTotal = document.querySelector("[id='subtotal']");
+let $tip = document.querySelector("input[name='tip']");
+let $tipWarn = document.querySelector(".tip-select .warning");
+let $orderWarn = document.querySelector(".input-one .warning");
 
-let $output = document.querySelector('.js-exercise output');
+let $output = document.querySelector(".js-exercise output");
 $output.hidden = true;
 
 $form.addEventListener('submit', function(event) {
@@ -79,21 +78,40 @@ $form.addEventListener('submit', function(event) {
 
 	$output.hidden = false;
 
-	var subTotal = parseInt($subTotal.value);
-	var subTotalFormatted = subTotal.toLocaleString('en-US');
+	let subTotal = parseInt($subTotal.value);
+	let subTotalFormatted = subTotal.toLocaleString("en-US");
 
-	var tip = document.querySelector("input[name='tip']:checked").value;
+	let tipElement = document.querySelector("input[name='tip']:checked");
+	var tip;
 
-	console.log(tip);
+	if ($subTotal.value === "") {
+		$orderWarn.innerHTML = "Did you order anything?";
+
+		$output.hidden = true;
+	} else {
+		$orderWarn.innerHTML = "";
+	}
+
+	if (tipElement !== null ) {
+		tip = tipElement.value;
+		$tipWarn.innerHTML = "";
+	} else {
+		$tipWarn.innerHTML = "You're not gonna tip?";
+		$output.hidden = true;
+		tip = ""
+	}
+
+	console.log("tip ", tip);
 	
-	var tipTotal = (subTotal * tip).toFixed(2);
-	var tipTotalFormatted = tipTotal.toLocaleString('en-US');
+	let tipTotal = (subTotal * tip).toFixed(2);
+	let tipTotalFormatted = tipTotal.toLocaleString('en-US');
 
-	var grandTotal = parseFloat(subTotal) + parseFloat(tipTotal);
-	var grandTotalFormatted = grandTotal.toLocaleString('en-US');
-
+	let grandTotal = parseFloat(subTotal) + parseFloat(tipTotal);
+	let grandTotalFormatted = grandTotal.toLocaleString('en-US');
 
 	console.log(tipTotal + " " + subTotal);
+
+	
 
 	$output.innerHTML = `
 		<p class="normal-voice">Sub-total: $${subTotalFormatted}</p> 
