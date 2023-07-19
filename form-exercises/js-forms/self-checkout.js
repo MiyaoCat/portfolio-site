f2df00ccaddd8a2f6d9c536e1cc36cb84bc5da06
john <jjmiyao@gmail.com>
2023-07-18 23:01:05 -0700

    Updated form; output message; paired labels with inputs

--- form-exercises/js-forms/self-checkout.js
+++ form-exercises/js-forms/self-checkout.js
@@ -11,32 +11,29 @@
 		 	
 		 	<div class="item-wrap">
 		 		<quantity>	
-				 	<p class="calm-voice">Quantity</p>
+				 	<label for="quantity">Quantity</label>
 					<input 
 						type="number" 
-						name="quantity" 
+						id="quantity" 
 						value=""
 						min="0"
 						placeholder="0"
 						label="write your number"
-					>
-		
-					<p class="calm-voice warning"></p>		
-						
+					>	
+					<p class="calm-voice warning"></p>								
 				</quantity>		
 		
 				<price>
-					<p class="calm-voice">Price</p>	
+					<label for="price">Price</label>
 					<input 
 						type="number" 
-						name="price" 
+						id="price" 
 						value=""
 						step= '0.01'
 						min='0'
-						placeholder="0.00">
-
+						placeholder="0.00"
+					>
 					<p class="calm-voice warning"></p>
-
 				</price>
 		 	</div>
 		</div>
@@ -46,31 +43,29 @@
 		
 		 	<div class="item-wrap">
 		 		<quantity>	
-				 	<p class="calm-voice">Quantity</p>
+				 	<label for="price">Quantity</label>
 					<input 
 						type="number" 
-						name="quantity2" 
+						id="quantity2" 
 						value=""
 						min="0"
 						placeholder="0"
-						label="write your number">
-
+					>
 					<p class="calm-voice warning"></p>
 						
 				</quantity>		
 		
 				<price>
-					<p class="calm-voice">Price</p>	
+					<label for="price2">Price</label>
 					<input 
 						type="number" 
-						name="price2" 
+						id="price2" 
 						value=""
 						step= '0.01'
 						min='0'
-						placeholder="0.00">
-		
-					<p class="calm-voice warning"></p>
-						
+						placeholder="0.00"
+					>	
+					<p class="calm-voice warning"></p>					
 				</price>
 		 	</div>
 		
@@ -81,29 +76,29 @@
 		
 		 	<div class="item-wrap">
 		 		<quantity>	
-				 	<p class="calm-voice">Quantity</p>
+				 	<label for="quantity3">Quantity</label>
 					<input 
 						type="number" 
-						name="quantity3" 
+						id="quantity3" 
 						value=""
 						min="0"
 						placeholder="0"
-						label="write your number">
-
-						<p class="calm-voice warning"></p>
+						label="write your number"
+					>
+					<p class="calm-voice warning"></p>
 						
 				</quantity>		
 		
 				<price>
-					<p class="calm-voice">Price</p>	
+					<label for="price3">Price</label>
 					<input 
 						type="number" 
-						name="price3" 
+						id="price3" 
 						value=""
 						step= '0.01'
 						min='0'
-						placeholder="0.00">		
-					
+						placeholder="0.00"
+					>		
 					<p class="calm-voice warning"></p>						
 				</price>
 		 	</div>
@@ -114,28 +109,28 @@
 			
 			<div class="item-wrap">	
 				<quantity>	
-				 	<p class="calm-voice">Quantity</p>
+				 	<label for="quantity4">Quantity</label>
 					<input 
 						type="number" 
-						name="quantity4" 
+						id="quantity4" 
 						value=""
 						min="0"
 						placeholder="0"
-						label="write your number">
-									
+						label="write your number"
+					>									
 					<p class="calm-voice warning"></p>						
 				</quantity>		
 				
 				<price>
-					<p class="calm-voice">Price</p>	
+					<label for="price4">Price</label>
 					<input 
 						type="number" 
-						name="price4" 
+						id="price4" 
 						value=""
 						step= '0.01'
 						min='0'
-						placeholder="0.00">				
-					
+						placeholder="0.00"
+					>									
 					<p class="calm-voice warning"></p>					
 				</price>
 			</div>
@@ -169,60 +164,70 @@
 </div>
 
 <script>
-var $selfCheckout = document.querySelector("form");
+	const $selfCheckout = document.querySelector("form");
 
-var $quantity1 = document.querySelector("[name='quantity']");
-var $price1 = document.querySelector("[name='price']");
+	const $quantity1 = document.querySelector("[id='quantity']");
+	const $price1 = document.querySelector("[id='price']");
 
-var $quantity2 = document.querySelector("[name='quantity2']");
-var $price2 = document.querySelector("[name='price2']");
+	const $quantity2 = document.querySelector("[id='quantity2']");
+	const $price2 = document.querySelector("[id='price2']");
 
-var $quantity3 = document.querySelector("[name='quantity3']");
-var $price3 = document.querySelector("[name='price3']");
+	const $quantity3 = document.querySelector("[id='quantity3']");
+	const $price3 = document.querySelector("[id='price3']");
 
-var $quantity4 = document.querySelector("[name='quantity4']");
-var $price4 = document.querySelector("[name='price4']");
+	const $quantity4 = document.querySelector("[id='quantity4']");
+	const $price4 = document.querySelector("[id='price4']");
 
-let $output = document.querySelector('.js-exercise output');
-$output.hidden = true;
-
-const taxRate = .1025;
-const taxPretty = taxRate*100;
-document.getElementById('taxRate').textContent = taxPretty;
-
-$selfCheckout.addEventListener('submit', function(myEvent) {
-	myEvent.preventDefault();
-	$output.hidden = false;
-	console.log("button pushed");
-
-	let quantity1 = $quantity1.value;
-	let price1 = $price1.value;
-
-	let quantity2 = $quantity2.value;
-	let price2 = $price2.value;
-
-	let quantity3 = $quantity3.value;
-	let price3 = $price3.value;
-
-	let quantity4 = $quantity4.value;
-	let price4 = $price4.value;
-
-	let subTotal = (quantity1 * price1) + (quantity2 * price2) + (quantity3 * price3) + (quantity4 * price4);
-	const subTotalFormatted = subTotal.toLocaleString('en-US');
+	const $output = document.querySelector('.js-exercise output');
+	$output.hidden = true;
 
 	const taxRate = .1025;
-	document.getElementById('taxRate').textContent = taxRate;
+	const taxPretty = taxRate*100;
+	document.getElementById('taxRate').textContent = taxPretty;
 
-	const taxTotal = (subTotal * taxRate).toFixed(2).toLocaleString('en-US');
-	const taxTotalFormatted = taxTotal.replace(/\B(?=(\d{3})+(?!\d))/g, ',');
+	$selfCheckout.addEventListener('submit', function(myEvent) {
+		myEvent.preventDefault();
+		$output.hidden = false;
+		console.log("button pushed");
 
-	const grandTotal = parseFloat(subTotal) + parseFloat(taxTotal);
-	const grandTotalFormatted = grandTotal.toLocaleString('en-US');
+		let quantity1 = $quantity1.value;
+		let price1 = $price1.value;
 
-	$output.innerHTML = 
-		`<p class="normal-voice">Sub-total: $${subTotalFormatted}</p> 
-		 <p class="normal-voice">Tax total: $${taxTotalFormatted}</p> 
-		 <p class="normal-voice">Grand total: $${grandTotalFormatted}</p> 
-		`;
-});
+		let quantity2 = $quantity2.value;
+		let price2 = $price2.value;
+
+		let quantity3 = $quantity3.value;
+		let price3 = $price3.value;
+
+		let quantity4 = $quantity4.value;
+		let price4 = $price4.value;
+
+		let subTotal = (quantity1 * price1) + (quantity2 * price2) + (quantity3 * price3) + (quantity4 * price4);
+		const subTotalFormatted = subTotal.toLocaleString('en-US');
+
+		const taxRate = .1025;
+		document.getElementById('taxRate').textContent = taxRate;
+
+		const taxTotal = (subTotal * taxRate).toFixed(2).toLocaleString('en-US');
+		const taxTotalFormatted = taxTotal.replace(/\B(?=(\d{3})+(?!\d))/g, ',');
+
+		const grandTotal = parseFloat(subTotal) + parseFloat(taxTotal);
+		const grandTotalFormatted = grandTotal.toLocaleString('en-US');
+
+		$output.innerHTML = 
+			`<p class="normal-voice">Sub-total: $${subTotalFormatted}</p> 
+			 <p class="normal-voice">Tax total: $${taxTotalFormatted}</p> 
+			 <p class="normal-voice">Grand total: $${grandTotalFormatted}</p> 
+			`;
+
+		if((quantity1 == "" || quantity1 == 0) || (price1 == "" || price1 == 0)) {
+			if((quantity2 == "" || quantity2 == 0) || (price2 == "" || price2 == 0)) {
+				if((quantity3 == "" || quantity3 == 0) || (price3 == "" || price3 == 0)){
+					if ((quantity4 == "" || quantity4 == 0) || (price4 == "" || price4 == 0)) {
+						$output.innerHTML = `<p class="normal-voice">Nothing of nothing is...well...nothing.</p>`
+					}
+				}
+			}			
+		}
+	});
 </script>
