8c11e1254ad8c54440f88ebd9607b12885d24224
john <jjmiyao@gmail.com>
2023-07-27 20:44:56 -0700

    Added dynamic county tax

--- form-exercises/js-forms/multi-state-tax.js
+++ form-exercises/js-forms/multi-state-tax.js
@@ -6,7 +6,7 @@
 	<div class="state">
 		<label class="normal-voice" for="stateDropDown">Select a state</label>
 		<select id="stateDropDown">
-			<option id="state" value="">- - Select - -</option>
+			<option value="">- - Select - -</option>
 		</select>
 		
 	</div>
@@ -31,8 +31,6 @@
 	const $form = document.getElementById("multi-state-tax");
 	const $output = document.querySelector("output");
 	$output.hidden = true;
-
-	// // Sample data containing US states
 	
 	const stateDropDown = document.getElementById('stateDropDown');
 	const countyDropDown = document.getElementById('countyDropDown');
@@ -43,61 +41,105 @@
 		.then(function(response) {
 			return response.json();
 		})
-		.then(function(taxData) {
-			
+		.then(function(taxData) {			
 			console.log(taxData);
-				// //populate sate dropdown list
+
 			function populateStates(states) {
 				states.forEach(state => {
 					const option = document.createElement("option");
-					option.value = state.code;
+					option.value = state.id;
 					option.innerHTML = state.name;
 					stateDropDown.appendChild(option);
 				})
 			}
-
 			populateStates(taxData);
 
 			function populateCounties(counties) {
 				countyDropDown.innerHTML = "";
-				//this resets the county to default 
+
 				const defaultOption = document.createElement("option");
 				defaultOption.value = "";
 				defaultOption.textContent = "- - Select - -";
 				countyDropDown.appendChild(defaultOption);
 
-				//loop through counties and append
 				counties.forEach(county => {
 					const option = document.createElement("option");
 					option.value = county.id;
 					option.textContent = county.name;
 					countyDropDown.appendChild(option);
-				})
+				})				
 			}
 
 			//Check to see when state option is changed.
 			stateDropDown.addEventListener("change", function() {
 				const selectedState = stateDropDown.value;
+
+				const options = stateDropDown.querySelectorAll('option');
+
+				options.forEach(option => {
+					if (option.value === selectedState) {
+						option.setAttribute("selected", "selected");
+					} else {
+						option.removeAttribute("selected");
+					}
+				})
+
 				const state = taxData.find(state => {
-					return state.code = selectedState;
+					return state.id == selectedState;
 				});
 
 				if (state) {
 					$county.style.display = "flex";
-					populateCounties(state.counties);
+					populateCounties(state.counties);	
+
+					//Check to see when county option is changed.
+					countyDropDown.addEventListener("change", function() {
+						const options = countyDropDown.querySelectorAll('option');
+						const selectedCounty = countyDropDown.value;
+
+						options.forEach(option => {
+							if (option.value === selectedCounty) {
+								option.setAttribute("selected", "selected");
+							} else {
+								option.removeAttribute("selected");
+							}
+						})
+					})	
 				} else {
 					populateCounties([]);
-				}
+				}			
+			})
+
+			$form.addEventListener("submit", event => {
+				event.preventDefault();
+				$output.hidden = false;
+
+				const selectedState = stateDropDown.value;
+				const state = taxData.find(state => {
+					return state.id == selectedState;
+				});
+
+				const selectedCountyId = countyDropDown.value;
+				const selectedStateCode = stateDropDown.value;
+
+				// Find the selected county object within the selected state object
+				const selectedCounty = state.counties.find(county => county.id === selectedCountyId);
+				// let countyTax = selectedCounty.tax;
+				// Now you have the selected county object, and you can use it as needed
+				console.log(selectedCounty);
+				console.log(selectedCounty.tax);
+				let name = state.name;
+				let stateTax = state.tax;
+				
+				let countyTax = selectedCounty.tax;
+				$output.innerHTML = `${name} state tax: ${stateTax}. County tax: ${countyTax}`;
 			})
 		})
 		.catch(function(error) {
 			console.error('Error fetching JSON data:', error);
 		});
 
-	$form.addEventListener("submit", event => {
-		event.preventDefault();
-		$output.hidden = false;
-	})
+
 </script>
 
 
