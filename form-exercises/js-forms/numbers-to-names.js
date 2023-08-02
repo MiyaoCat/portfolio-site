36d00a521c36ab358e1fac44510d4bd34a6f276e
john <jjmiyao@gmail.com>
2023-08-01 17:40:17 -0700

    Added error messages

--- form-exercises/js-forms/numbers-to-names.js
+++ form-exercises/js-forms/numbers-to-names.js
@@ -37,58 +37,78 @@
 		event.preventDefault();
 
 		let number = parseFloat($number.value);
-		console.log(number);
-		let month;
+		// let month;
 
-		switch (number) {
-			case 1:
-				month = "1 = January";
-				break;
-			case 2:
-				month = "February";
-				break;
-			case 3:
-				month = "March";
-				break;
-			case 4:
-				month = "April";
-				break;	
-			case 5:
-				month = "May";
-				break;
-			case 6:
-				month = "June";
-				break;
-			case 7:
-				month = "July";
-				break;
-			case 8:
-				month = "August";
-				break;
-			case 9:
-				month = "September";
-				break;
-			case 10:
-				month = "October";
-				break;
-			case 11:
-				month = "November";
-				break;
-			case 12:
-				month = "December";
-				break;
-			default:
-				month = "There's only 12 months."			
-		}
+		// switch (number) {
+		// 	case 1:
+		// 		month = "January";
+		// 		break;
+		// 	case 2:
+		// 		month = "February";
+		// 		break;
+		// 	case 3:
+		// 		month = "March";
+		// 		break;
+		// 	case 4:
+		// 		month = "April";
+		// 		break;	
+		// 	case 5:
+		// 		month = "May";
+		// 		break;
+		// 	case 6:
+		// 		month = "June";
+		// 		break;
+		// 	case 7:
+		// 		month = "July";
+		// 		break;
+		// 	case 8:
+		// 		month = "August";
+		// 		break;
+		// 	case 9:
+		// 		month = "September";
+		// 		break;
+		// 	case 10:
+		// 		month = "October";
+		// 		break;
+		// 	case 11:
+		// 		month = "November";
+		// 		break;
+		// 	case 12:
+		// 		month = "December";
+		// 		break;
+		// 	default:
+		// 		month = "There's only 12 months."			
+		// }
 
-		$output.innerHTML = month;
+		const monthMap = new Map([
+			[1, "January"],
+			[2, "February"],
+			[3, "March"],
+			[4, "April"],
+			[5, "May"],
+			[6, "June"],
+			[7, "July"],
+			[8, "August"],
+			[9, "September"],
+			[10, "October"],
+			[11, "November"],
+			[12, "December"]
+		]);
 
-		if ($number.value !== "") {
-			$output.hidden = false;
-			$numberWarn.innerHTML = "";
-			$output.innerHTML = month;
+		let month = monthMap.get(number);
+
+		if ($number.value > 12) {
+			$output.hidden = true;
+			$numberWarn.innerHTML = "There's only 12 months, buster!"
+		}	
+		else if ($number.value == "") {
+			// $output.hidden = true;
+			$numberWarn.innerHTML = "You had one job."
 		} else {
-			$numberWarn.innerHTML = "Enter a number"
+			$numberWarn.innerHTML = "";
+			$output.hidden = false;
+			$output.innerHTML = month;
 		}
+		console.log($number.value)
 	})
 </script>
