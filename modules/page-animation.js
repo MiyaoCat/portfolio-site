d18807b3e13b158942de2714db139dc745a0e392
John Miyao <jjmiyao@gmail.com>
2023-08-31 23:11:10 -0700

    Changed page animations to specific pages

--- modules/page-animation.js
+++ modules/page-animation.js
@@ -6,60 +6,60 @@ var childElements = Array.from(bodyElement.children);
 var elementsToAnimate = childElements.slice(3, -3);
 var footer = gsap.timeline();
 
-pageHeader
-	.from('.title1', {
+const pages = document.querySelectorAll(".home, .contact, .about, .projects, .project, .exercises, .style-guide, .resume");
+
+if (pages.length > 0) {
+	pageHeader
+		.from('.title1', {
+			opacity: 0,
+			x: -1000,
+			duration: 1,
+			ease: "back.out(.75)",
+	   }, .2)
+		.from('.title2', {
+			opacity: 0,
+			x: 1000,
+			duration: 1,
+			ease: "back.out(.75)"
+	  	}, .4);
+
+	gsap.from(elementsToAnimate, {
 		opacity: 0,
-		x: -1000,
+		x: 1500,
 		duration: 1,
 		ease: "back.out(.75)",
-   }, .2)
-	.from('.title2', {
-		opacity: 0,
-		x: 1000,
-		duration: 1,
-		ease: "back.out(.75)"
-  }, .4);
+		stagger: 0.2
+	}, .2)
 
-gsap.from(elementsToAnimate, {
-	opacity: 0,
-	x: 1500,
-	duration: 2,
-	ease: "back.out(.75)",
-	stagger: 0.2
-}, .2)
+	if (window.innerWidth <= 700) {
+		background
+		.from('.background', {
+			y: 0,
+			duration: 0,
+		})
+		pageHeader
+		.from('.title1', {
+			x: -1000,
+			duration: 1,
+			ease: "back.out(.75)"
+	   }, .1)
+		.from('.title2', {
+			x: 1000,
+			duration: 1,
+			ease: "back.out(.75)"
+	  }, .2)
 
-// gsap.to(elementsToAnimate, {
-// 	onComplete: function() {
-// 		gsap.set(elementsToAnimate, { clearProps: "all" });
-// 	}
-// })
-if (window.innerWidth <= 700) {
-	background
-	.from('.background', {
-		y: 0,
-		duration: 0,
-	})
-	pageHeader
-	.from('.title1', {
-		x: -1000,
-		duration: 1,
-		ease: "back.out(.75)"
-   }, .1)
-	.from('.title2', {
-		x: 1000,
-		duration: 1,
-		ease: "back.out(.75)"
-  }, .2)
-
-} else {
-	background
-	.from('.background', {
-		x: -1000,
-		duration: .75,
-		ease: "power2.out"
-	})
-	pageHeader.delay(.5)
+	} else {
+		background
+		.from('.background', {
+			x: -1000,
+			duration: .75,
+			ease: "power2.out"
+		})
+		pageHeader.delay(.5)
+	}
 }
+
 
 footer 
 	.from("footer", {
